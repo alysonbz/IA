6cd87a988ec74ffc1f882316a28c029e9055beed
Sheldaa <sheldasouza@alu.ufc.br>
2023-03-29 17:37:18 -0300

    minha primeira modificação no git. :)

--- Pre-processing/Unidade1/0_class_anotation.py
+++ Pre-processing/Unidade1/0_class_anotation.py
@@ -7,5 +7,28 @@ wine  = load_wine_dataset()
 df1 = load_df1_unidade1()
 df2 = load_df2_unidade1()
 
+###print(hiking.head())
 
-print(volunteer['hits'].dtype)
+###print(hiking.info())
+
+###print(wine.describe())
+
+###print(df1)
+
+###print(df1.dropna())
+
+###print(df1.drop([1,2,3]))
+
+###print(df1.drop("A", axis=1))
+
+###print(df1.isna().sum()) pergunta onde existe NaN e soma quantos tem em cada coluna.
+
+### print(df1.dropna(subset=['B']))
+
+### print(df1.dropna(thresh=2)) deleta NaN nas linhas de acordo com a quantidade limite.
+
+###print(volunteer.info())
+###print(df2)
+###print(df2.info())
+###df2["C"] = df2["C"].astype("int64") #transformando os atributos da coluna C em inteiros
+###print(df2.dtypes) #usa-se dtypes para ver os tipos de dados contidos no dataset
