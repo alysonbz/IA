290871162c11a8812d41b07b43ac8d197327e677
alekyne <aryanialekyni@gmail.com>
2023-03-29 14:39:03 -0300

    Prazer povo, fiz minhas primeiras alterações no git! ;)

--- Pre-processing/Unidade1/0_class_anotation.py
+++ Pre-processing/Unidade1/0_class_anotation.py
@@ -1,4 +1,3 @@
-from src.utils import load_hiking_dataset , load_df2_unidade1,load_wine_dataset, load_df1_unidade1, load_volunteer_dataset
 import pandas as pd
 
 volunteer = load_volunteer_dataset()
@@ -8,4 +7,63 @@ df1 = load_df1_unidade1()
 df2 = load_df2_unidade1()
 
 
-print(volunteer['hits'].dtype)
+"""print(volunteer['hits'].dtype)"""
+#print(info)
+
+
+#print(df1)
+
+#print(df1.drop("A", axis=1))
+
+#print(df1.isna().sum())
+
+#print(df1.dropna(subset=["B"]))
+
+#print(df1.dropna(thresh=2))
+
+#print(volunteer.info())
+
+
+
+
+#print(df2)
+
+#print(df2.info())
+
+
+df2["C"] = df2["C"].astype("int64")
+print(df2.dtypes)import pandas as pd
+
+volunteer = load_volunteer_dataset()
+hiking = load_hiking_dataset()
+wine  = load_wine_dataset()
+df1 = load_df1_unidade1()
+df2 = load_df2_unidade1()
+
+
+"""print(volunteer['hits'].dtype)"""
+#print(info)
+
+
+#print(df1)
+
+#print(df1.drop("A", axis=1))
+
+#print(df1.isna().sum())
+
+#print(df1.dropna(subset=["B"]))
+
+#print(df1.dropna(thresh=2))
+
+#print(volunteer.info())
+
+
+
+
+#print(df2)
+
+#print(df2.info())
+
+
+df2["C"] = df2["C"].astype("int64")
+print(df2.dtypes)
--- Pre-processing/Unidade1/2_data_types.py
+++ Pre-processing/Unidade1/2_data_types.py
@@ -2,14 +2,15 @@ from src.utils import load_volunteer_dataset
 
 volunteer = load_volunteer_dataset()
 
-# Print os primeiros elementos da coluna hits
-___
+print("Print os primeiros elementos da coluna hits")
+print(volunteer['hits'].head())
 
-# Print as caracteristicas da coluna hits
-__
 
-# Converta a coluna hits para o tipo int
-___
+print("Print as caracteristicas da coluna hits")
+print(volunteer['hits'].info())
 
-# Print as caracteristicas da coluna hits novamente
----
+print("Converta a coluna hits para o tipo int")
+print(volunteer["hits"].dtypes)
+
+print("Print as caracteristicas da coluna hits novamente")
+print(volunteer['hits'].info)
