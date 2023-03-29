16e9cc12c7fc45e9b22ef281fce6e04f75607a03
Matheus Rabelo <matheusrabelo@alu.ufc.br>
2023-03-29 14:36:06 -0300

    Minha primeira modificação no git.

--- Pre-processing/Unidade1/0_class_anotation.py
+++ Pre-processing/Unidade1/0_class_anotation.py
@@ -7,5 +7,17 @@ wine  = load_wine_dataset()
 df1 = load_df1_unidade1()
 df2 = load_df2_unidade1()
 
+## print(hiking.head())
+## print(hiking.info())
+## print(wine.describe)
+## print(df1.dropna())
+### print(df1.drop([1,2,3]))   ### dropa as linhas 1,2,3
+### print(df1.drop("A", axis=1))  ##dropa o eixo A
+### print(df1.isna().sum())   ##Soma os NA de todas as colunas
+### print(df1.dropna(subset=["B"]))
+### print(df1.dropna(thresh=2))  ## exlcui as linhas com mais de 2 NA
 
-print(volunteer['hits'].dtype)
+### print(volunteer.info()) #INFORMAÇAÕES DO DATAS SET
+### print(df2.info())
+### df2["C"] = df2["C"].astype("int64")
+### print(df2.dtypes)
