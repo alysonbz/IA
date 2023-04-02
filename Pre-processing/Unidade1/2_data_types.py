b1e8dd2111cf8e028027aeb2e5f2f792480c5b47
henriquesantos769 <henripa224444@gmail.com>
2023-04-02 11:08:12 -0300

    Resposta da Questão Data_type

--- Pre-processing/Unidade1/2_data_types.py
+++ Pre-processing/Unidade1/2_data_types.py
@@ -3,13 +3,13 @@ from src.utils import load_volunteer_dataset
 volunteer = load_volunteer_dataset()
 
 # Print os primeiros elementos da coluna hits
-
+print(volunteer['hits'].head())
 
 # Print as caracteristicas da coluna hits
-__
+print(volunteer['hits'].describe())
 
 # Converta a coluna hits para o tipo int
-___
+volunteer['hits'] = volunteer['hits'].astype(int)
 
 # Print as caracteristicas da coluna hits novamente
----
+print(volunteer['hits'].info())
