d171d08fbba93a5a5a8ba0cc8dba6fdffe93872d
Suziane <suzianebrandao90@alu.ufc.br>
2023-04-03 11:14:36 -0300

    teste de modificação

--- Pre-processing/Unidade1/1_pre_processing.py
+++ Pre-processing/Unidade1/1_pre_processing.py
@@ -3,21 +3,21 @@ import pandas as pd
 volunteer = pd.DataFrame('dataset/archive/opportunities.csv')
 
 # Mostre a dimensão do dataset volunteer
-print(___)
+print(shape(volunteer))
 
 #mostre os tipos de dados existentes no dataset
-___
+#___
 
 #mostre quantos elementos do dataset estão faltando na coluna
----
+#---
 
 # Exclua as colunas Latitude e Longitude de volunteer
-volunteer_cols =
+#volunteer_cols =
 
 # Exclua as linhas com valores null da coluna category_desc de volunteer_cols
-volunteer_subset =
+#volunteer_subset =
 
 # Print o shape do subset
-___
+#___
 
 
