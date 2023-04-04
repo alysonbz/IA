e2d7f0f75557293667ddec4f22bc38f8bba03707
Luciana <lucianasmartins779@gmail.com>
2023-04-04 15:10:00 -0300

    aula IA- 04.04.23

--- Pre-processing/Unidade1/3_training_and_test_sets.py
+++ Pre-processing/Unidade1/3_training_and_test_sets.py
@@ -1,24 +1,25 @@
 from src.utils import load_volunteer_dataset
+from sklearn.model_selection import train_test_split
 
 volunteer = load_volunteer_dataset()
 
 # Exclua as colunas Latitude e Longitude de volunteer
-#volunteer_new = volunteer.drop(["Latitude", "Longitude"], axis=1)
+volunteer_new = volunteer.drop(["Latitude", "Longitude"], axis=1)
 
 # Exclua as linhas com valores null da coluna category_desc de volunteer_new
-#print(volunteer["category_desc"].dropna())
+volunteer_new= volunteer_new["category_desc"].dropna()
 
 # mostre o balanceamento das classes em 'category_desc'
-#print(volunteer['category_desc'].__,'\n','\n')
+print(volunteer['category_desc'].value_counts(),'\n','\n')
 
 # Crie um DataFrame com todas as colunas, com exceção de ``category_desc``
-x = volunteer.drop("category_desc", axis=1)
-print(x)
+X= volunteer.drop("category_desc", axis=1)
 
 # Crie um dataframe de labels com a coluna category_desc
-#y = __[['__']]
+y = volunteer[['category_desc']]
 
 # # Utiliza a a amostragem stratificada para separar o dataset em treino e teste
-#X_train, X_test, y_train, y_test = (, , stratify=, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=y, random_state=42)
 
 # mostre o balanceamento das classes em 'category_desc' novamente
+print(y_train['category_desc'].value.counts())
