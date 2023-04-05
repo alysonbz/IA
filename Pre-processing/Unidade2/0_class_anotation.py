1d5390fc1e3d9ea4e85fdf6aec786e2648a0547d
Luis_Savio <luissavio150@gmail.com>
2023-04-05 18:01:32 -0300

    Aula 3

--- Pre-processing/Unidade2/0_class_anotation.py
+++ Pre-processing/Unidade2/0_class_anotation.py
@@ -1,5 +1,20 @@
 from src.utils import  load_df1_unidade2,load_df2_unidade2
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+
 
 df1 = load_df1_unidade2()
 df2 = load_df2_unidade2()
 
+#print(df1)
+#print(df1.var())
+#df1["log_2"] = np.log(df1["col2"])
+#print(df1)
+
+#print(df1[["col1", "log_2"]].var())
+
+scaler = StandardScaler()
+df2_scaled = pd.DataFrame(scaler.fit_transform(df2),columns =df2.columns)
+print(df2_scaled)
+print(df2_scaled.var())
--- Pre-processing/Unidade2/1_standardization.py
+++ Pre-processing/Unidade2/1_standardization.py
@@ -1,3 +1,4 @@
+import numpy as np
 from src.utils import load_wine_dataset
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
@@ -5,16 +6,17 @@ from sklearn.model_selection import train_test_split
 wine = load_wine_dataset()
 
 X = wine.drop(['Quality'],axis=1)
+X = np.log(X)
 
 y = wine['Quality'].values
 
 # divida o dataset em treino e teste
-X_train, X_test, y_train, y_test = ____(____, ____, stratify=____, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, random_state=42)
 
 knn = KNeighborsClassifier()
 
 # Aplique a função fit do knn
-knn.____(____, ____)
+knn.fit(X_train, y_train)
 
 # mostre o acerto do algoritmo
-print(knn.____(____))
+print(knn.score(X_test, y_test))
