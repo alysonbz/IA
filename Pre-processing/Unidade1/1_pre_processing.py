401e679d23537bdfab392ab6be51922fad0204ca
Bia14k <maria.bianca.mb0@gmail.com>
2023-04-11 15:38:21 -0300

    resolvendo as atividades atrasadas

--- Pre-processing/Unidade1/1_pre_processing.py
+++ Pre-processing/Unidade1/1_pre_processing.py
@@ -7,14 +7,13 @@ volunteer = load_volunteer_dataset()
 print(volunteer.shape)
 
 #mostre os tipos de dados existentes no dataset
-print(volunteer.info)
+print(volunteer.info())
 
 #mostre quantos elementos do dataset estão faltando na coluna
-print(volunteer.locality)
+print(volunteer["locality"].isnull().sum())
 
 # Exclua as colunas Latitude e Longitude de volunteer
-volunteer_cols = volunteer.drop(columns=["Latitude","Longitude"])
-print(volunteer_cols.shape)
+volunteer_cols = volunteer.drop(["Latitude","Longitude"], axis=1)
 
 # Exclua as linhas com valores null da coluna category_desc de volunteer_cols
 volunteer_subset = volunteer_cols.dropna(subset=["category_desc"])
--- Pre-processing/Unidade1/2_data_types.py
+++ Pre-processing/Unidade1/2_data_types.py
@@ -3,13 +3,14 @@ from src.utils import load_volunteer_dataset
 volunteer = load_volunteer_dataset()
 
 # Print os primeiros elementos da coluna hits
-print()
+print(volunteer["hits"].head())
 
 # Print as caracteristicas da coluna hits
-
+print(volunteer["hits"].info())
 
 # Converta a coluna hits para o tipo int
-
+volunteer["hits"] = volunteer["hits"].astype("int32")
+print(volunteer.dtypes)
 
 # Print as caracteristicas da coluna hits novamente
-
+print(volunteer["hits"].info())
