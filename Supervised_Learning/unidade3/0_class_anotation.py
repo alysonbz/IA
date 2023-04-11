f3575579c89fd3ad1d86ac3acf8892c9151967fc
Emily Mendonça <emilymendc@alu.ufc.br>
2023-04-11 14:09:43 -0300

    Revert "Merge branch 'main' of https://github.com/alysonbz/IA into emily_mendonça"
    This reverts commit 0dc39495edd7c8ea78252e5839ddc909ce7d9daa, reversing
    changes made to a5ed960927c5e9a2dfdb93930a4796b1447b7e8d.

--- Supervised_Learning/unidade3/0_class_anotation.py
+++
@@ -1,7 +0,0 @@
-from src.utils import load_churn_dataset
-from sklearn.neighbors import KNeighborsClassifier
-import numpy as np
-
-churn_df = load_churn_dataset()
-
-
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,4 @@
 import pandas as pd
-from sklearn.preprocessing import LabelEncoder
 
 
 def load_volunteer_dataset():
@@ -25,9 +24,3 @@ def load_df1_unidade2():
 def load_df2_unidade2():
     return pd.read_csv('../dataset/df2_unidade2.csv')
 
-def load_churn_dataset():
-    df = pd.read_csv('../dataset/churn_train.csv')
-    le = LabelEncoder()
-    df['churn'] = le.fit_transform(df['churn'])
-    df['churn'] = le.fit_transform(df['churn'])
-    return df
