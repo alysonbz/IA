5d29141893b21da10a84e4f96850cc0261d7f514
Bia14k <maria.bianca.mb0@gmail.com>
2023-04-12 13:25:15 -0300

    testando o KNN

--- Pre-processing/Unidade1/3_training_and_test_sets.py
+++ Pre-processing/Unidade1/3_training_and_test_sets.py
@@ -7,18 +7,19 @@ volunteer = load_volunteer_dataset()
 volunteer_new = volunteer.drop(["Latitude","Longitude"], axis=1)
 
 # Exclua as linhas com valores null da coluna category_desc de volunteer_new
-volunteer = volunteer["category_desc"].
-print(volunteer)
+volunteer = volunteer_new.dropna(subset=["category_desc"])
+
 # mostre o balanceamento das classes em 'category_desc'
-#print(___['category_desc'].__,'\n','\n')
+#print(volunteer['category_desc'].value_counts(),'\n','\n') #contando por categoria
 
 # Crie um DataFrame com todas as colunas, com exceção de ``category_desc``
-#X = volunteer.__(__, axis=1)
+X = volunteer.drop("category_desc", axis=1)
 
 # Crie um dataframe de labels com a coluna category_desc
-#y = __[['__']]
+y = volunteer[['category_desc']]
 
 # # Utiliza a a amostragem stratificada para separar o dataset em treino e teste
-#X_train, X_test, y_train, y_test = __(__, __, stratify=__, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X,y, stratify=y, random_state=42)
 
 # mostre o balanceamento das classes em 'category_desc' novamente
+print(volunteer['category_desc'].value_counts(),'\n','\n')
--- Pre-processing/Unidade2/0_class_anotation.py
+++ Pre-processing/Unidade2/0_class_anotation.py
@@ -1,5 +1,22 @@
+import pandas as pd
+
 from src.utils import  load_df1_unidade2,load_df2_unidade2
 
-df1 = load_df1_unidade2()
-df2 = load_df2_unidade2()
 
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+
+iris = pd.read_csv('iris.data.csv')
+print(iris.value_counts())
+
+
+
+# divida o dataset em treino e teste
+#X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, random_state=42)
+knn = KNeighborsClassifier()
+
+# Aplique a função fit do knn
+#knn.fit(X_train, y_train)
+
+# mostre o acerto do algoritmo
+#print(knn.score(X_test,y_test))
