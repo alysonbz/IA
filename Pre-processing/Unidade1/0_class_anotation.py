a8b97e4f444325e7b3435e39dfa9dc8749ac22de
MASTER <aryaanialekyni@gmail.com>
2023-04-12 13:00:53 -0300

    Editando

--- Pre-processing/Unidade1/0_class_anotation.py
+++ Pre-processing/Unidade1/0_class_anotation.py
@@ -1,4 +1,11 @@
 import pandas as pd
+from src.utils import load_volunteer_dataset
+from src.utils import load_hiking_dataset
+from src.utils import load_wine_dataset
+from src.utils import load_df1_unidade1
+from src.utils import load_df2_unidade1
+
+
 
 volunteer = load_volunteer_dataset()
 hiking = load_hiking_dataset()
@@ -8,41 +15,6 @@ df2 = load_df2_unidade1()
 
 
 """print(volunteer['hits'].dtype)"""
-#print(info)
-
-
-#print(df1)
-
-#print(df1.drop("A", axis=1))
-
-#print(df1.isna().sum())
-
-#print(df1.dropna(subset=["B"]))
-
-#print(df1.dropna(thresh=2))
-
-#print(volunteer.info())
-
-
-
-
-#print(df2)
-
-#print(df2.info())
-
-
-df2["C"] = df2["C"].astype("int64")
-print(df2.dtypes)import pandas as pd
-
-volunteer = load_volunteer_dataset()
-hiking = load_hiking_dataset()
-wine  = load_wine_dataset()
-df1 = load_df1_unidade1()
-df2 = load_df2_unidade1()
-
-
-"""print(volunteer['hits'].dtype)"""
-#print(info)
 
 
 #print(df1)
@@ -67,3 +39,8 @@ df2 = load_df2_unidade1()
 
 df2["C"] = df2["C"].astype("int64")
 print(df2.dtypes)
+
+
+
+
+
