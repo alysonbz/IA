83501d2303a2a8932b74cb7dfe399b0215cf6f59
Luis_Savio <luissavio150@gmail.com>
2023-04-18 18:59:50 -0300

    Atividades pendentes

--- Pre-processing/Unidade1/2_data_types.py
+++ Pre-processing/Unidade1/2_data_types.py
@@ -3,13 +3,14 @@ from src.utils import load_volunteer_dataset
 volunteer = load_volunteer_dataset()
 
 # Print os primeiros elementos da coluna hits
-___
+print(volunteer['hits'].head(5))
 
 # Print as caracteristicas da coluna hits
-__
+print(volunteer['hits'].describe())
 
 # Converta a coluna hits para o tipo int
-___
+
+volunteer['hits'] = volunteer['hits'].astype(int)
 
 # Print as caracteristicas da coluna hits novamente
----
+print(volunteer['hits'])
--- Pre-processing/Unidade2/0_class_anotation.py
+++ Pre-processing/Unidade2/0_class_anotation.py
@@ -7,12 +7,12 @@ from sklearn.preprocessing import StandardScaler
 df1 = load_df1_unidade2()
 df2 = load_df2_unidade2()
 
-#print(df1)
-#print(df1.var())
-#df1["log_2"] = np.log(df1["col2"])
-#print(df1)
+print(df1)
+print(df1.var())
+df1["log_2"] = np.log(df1["col2"])
+print(df1)
 
-#print(df1[["col1", "log_2"]].var())
+print(df1[["col1", "log_2"]].var())
 
 scaler = StandardScaler()
 df2_scaled = pd.DataFrame(scaler.fit_transform(df2),columns =df2.columns)
