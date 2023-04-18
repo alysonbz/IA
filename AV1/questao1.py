1a316781e95dd2ab522757bee1b633d3f1ff2a8e
Bia14k <maria.bianca.mb0@gmail.com>
2023-04-18 15:05:23 -0300

    começo da prova

--- AV1/questao1.py
+++ AV1/questao1.py
@@ -1,14 +1,18 @@
 #importe as bibliotecas necessárias
 
+import pandas as pd
+import numpy as np
+
 
 ## Carregue o dataset definido para você
-
-
+diabetes = pd.read_csv("..\AV1\dataset\diabetes_012_health_indicators_BRFSS2015.csv")
+print(diabetes)
 
 # Verifique se existem celulas vazias ou Nan. Se existir, excluir e criar um novo dataframe.
-
-
-
+print(diabetes.head())
+print(diabetes.isna().sum())
+print(diabetes.isnull().sum())
+print(diabetes.info)
 # Verifique quais colunas são as mais relevantes e crie um novo dataframe.
 
 
