d7d15ca733f02e69d5420f2d295787e806511404
dcgiovanna <dcgiovanna@alu.ufc.br>
2023-04-18 15:13:44 -0300

    18-04, início AV1

--- AV1/questao1.py
+++ AV1/questao1.py
@@ -1,17 +1,17 @@
 #importe as bibliotecas necessárias
-
+import pandas as pd
 
 ## Carregue o dataset definido para você
-
-
+df = pd.read_csv(r'C:\Users\LAB1_00\Documents\GD\IA\AV1\dataset\dataset__binary.csv')
 
 # Verifique se existem celulas vazias ou Nan. Se existir, excluir e criar um novo dataframe.
-
-
+print(df.isna().sum())
+print(df.isnull().sum())
 
 # Verifique quais colunas são as mais relevantes e crie um novo dataframe.
-
-
+print(df.info())
+print(df.head())
+print(df[["var_1", "var_2"]].value_counts())
 
 #Print o dataframe final e mostre a distribuição de classes que você deve classificar
 
@@ -21,4 +21,4 @@
 
 
 
-#Salve o dataset atualizado se houver modificações.
+#Salve o dataset atualizado se houver modifcações.i
