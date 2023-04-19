5d8c3f796642f8ee3541a9b45567021cbc738ef9
livia094 <sousalivia257@gmail.com>
2023-04-19 17:57:22 -0600

    att

--- AV1/questao2.py
+++ AV1/questao2.py
@@ -2,7 +2,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
-import matplotlib.pyplot as plt
+
 
 #Carregue o dataset. Se houver o dataset atualizado, carregue o atualizado.
 cancer = pd.read_csv('dados_preprocessados.csv')
--- AV1/questao3.py
+++ AV1/questao3.py
@@ -2,6 +2,8 @@
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
 
 
 #Carregue o dataset. Se houver o dataset atualizado, carregue o atualizado.
@@ -10,12 +12,14 @@ print(cancer)
 X = pd.read_csv('dados_preprocessados2.csv')
 y = pd.read_csv('dados_preprocessados3.csv')
 
+
 #Normalize o conjunto de dados com normalização logarítmica e verifique a acurácia do knn.
-scaler = StandardScaler()
-X_norm = scaler.fit_transform(X)
+X_norm = np.log(X)
+print(X_norm)
 
+knn = KNeighborsClassifier(6)
 
-
+print('score', knn.score(X_test, y_test))
 #Normalize o conjunto de dados com normalização de media zero e variância unitária e e verifique a acurácia do knn.
 
 
