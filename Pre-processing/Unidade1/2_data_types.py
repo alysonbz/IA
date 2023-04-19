6b7eac202d0043fcfd6a781b5284a67d493a2a36
Castro-Thais-Andrade <castroandrade@alu.ufc.br>
2023-04-19 15:16:26 -0300

    unidade 1- modificação

--- Pre-processing/Unidade1/2_data_types.py
+++ Pre-processing/Unidade1/2_data_types.py
@@ -1,17 +1,17 @@
-from src.utils import load_volunteer_dataset
-from  sklearn.model_selection import  train_test_split
 
+from src.utils import load_volunteer_dataset
 
 volunteer = load_volunteer_dataset()
 
 # Print os primeiros elementos da coluna hits
-___
+print(volunteer["hits"].head())
 
 # Print as caracteristicas da coluna hits
-__
+print(volunteer["hits"].info())
 
 # Converta a coluna hits para o tipo int
-___
+volunteer["hits"] = volunteer["hits"].astype("int32")
+print(volunteer.dtypes)
 
 # Print as caracteristicas da coluna hits novamente
----
+print(volunteer["hits"].info())
