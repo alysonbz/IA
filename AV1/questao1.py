35b61157e1502d3d7b009ea2a405350f92f34922
livia094 <sousalivia257@gmail.com>
2023-04-18 18:06:14 -0600

    ap1

--- AV1/questao1.py
+++ AV1/questao1.py
@@ -1,12 +1,12 @@
 #importe as bibliotecas necessárias
-
+import pandas as pd
 
 ## Carregue o dataset definido para você
-
+read_cancer = pd.read_csv(r"C:\Users\LAB1_00\Documents\Livia\IA\AV1\dataset\Cancer_Data.csv")
 
 
 # Verifique se existem celulas vazias ou Nan. Se existir, excluir e criar um novo dataframe.
-
+print(read_cancer.isnull().sum())
 
 
 # Verifique quais colunas são as mais relevantes e crie um novo dataframe.
