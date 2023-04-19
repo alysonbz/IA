1cb0a44b8a400f3f19963f78bab817b940650dc9
Luan Barroso <luanbarroso1212@gmail.com>
2023-04-19 09:12:46 -0300

    finalmente terminando atrasada essas incriveis questões que consumiu minha sanidade mental para completar ;)

--- Pre-processing/Unidade1/3_training_and_test_sets.py
+++ Pre-processing/Unidade1/3_training_and_test_sets.py
@@ -1,24 +1,24 @@
 from src.utils import load_volunteer_dataset
-_____
-
+from sklearn.model_selection import train_test_split
 volunteer = load_volunteer_dataset()
 
 # Exclua as colunas Latitude e Longitude de volunteer
-volunteer_new =
+volunteer_new = volunteer.drop(["Latitude", "Longitude"], axis=1)
 
 # Exclua as linhas com valores null da coluna category_desc de volunteer_new
-volunteer =
+volunteer = volunteer_new.dropna(subset=["category_desc"])
 
 # mostre o balanceamento das classes em 'category_desc'
-#print(['category_desc'].__,'\n','\n')
+print(volunteer['category_desc'].value_counts(),'\n')
 
 # Crie um DataFrame com todas as colunas, com exceção de ``category_desc``
-#X = volunteer.(, axis=1)
+X = volunteer.drop('category_desc', axis=1)
 
 # Crie um dataframe de labels com a coluna category_desc
-#y = [['']]
+y =volunteer[['category_desc']]
 
 # # Utiliza a a amostragem stratificada para separar o dataset em treino e teste
-#X_train, X_test, y_train, y_test = __(__, __, stratify=__, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, random_state=42)
 
 # mostre o balanceamento das classes em 'category_desc' novamente
+print(y_train['category_desc'].value_counts(),'/n')
--- Pre-processing/Unidade2/1_standardization.py
+++ Pre-processing/Unidade2/1_standardization.py
@@ -1,6 +1,7 @@
 from src.utils import load_wine_dataset
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
+import numpy as pd
 
 wine = load_wine_dataset()
 
@@ -9,12 +10,12 @@ X = wine.drop(['Quality'],axis=1)
 y = wine['Quality'].values
 
 # divida o dataset em treino e teste
-X_train, X_test, y_train, y_test = ____(____, ____, stratify=____, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, random_state=42)
 
 knn = KNeighborsClassifier()
 
 # Aplique a função fit do knn
-knn.____(____, ____)
+knn.fit(X_train, y_train)
 
 # mostre o acerto do algoritmo
-print(knn.____(____))
+print(knn.score(X_test,y_test))
