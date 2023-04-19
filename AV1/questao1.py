9aaea190925cbd5758afa614f05cd9fae820601e
MASTER <aryaanialekyni@gmail.com>
2023-04-18 22:31:50 -0300

    prova

--- AV1/questao1.py
+++ AV1/questao1.py
@@ -1,16 +1,21 @@
 #importe as bibliotecas necessárias
-
+import pandas as pd
 
 ## Carregue o dataset definido para você
-
-
+cancer = pd.read_csv('breast-cancer.csv')
+print(cancer.head())
 
 # Verifique se existem celulas vazias ou Nan. Se existir, excluir e criar um novo dataframe.
-
-
+print(cancer.isnull().sum())  #quantos null
+print(cancer.isna().sum())  #quantos na
 
 # Verifique quais colunas são as mais relevantes e crie um novo dataframe.
+print(cancer.columns)
 
+corr = cancer.corr()
+print(corr)
+
+#cancer_new =
 
 
 #Print o dataframe final e mostre a distribuição de classes que você deve classificar
