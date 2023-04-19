4a09cee3dfb712a551a7f57cb69765bfd11d47eb
livia094 <sousalivia257@gmail.com>
2023-04-18 19:43:03 -0600

    att

--- AV1/questao1.py
+++ AV1/questao1.py
@@ -7,11 +7,12 @@ read_cancer = pd.read_csv(r"C:\Users\LAB1_00\Documents\Livia\IA\AV1\dataset\Canc
 
 # Verifique se existem celulas vazias ou Nan. Se existir, excluir e criar um novo dataframe.
 print(read_cancer.isnull().sum())
-X = read_cancer.dropna(subset=['diagnosis'], axis= 1)
-
-
+read_cancer['Unnamed: 32']
+read_cancer['diagnosis'].replace(['B', 'M'],
+                        [0, 1], inplace=True)
+read_cancer.head()
 # Verifique quais colunas são as mais relevantes e crie um novo dataframe.
-
+cancer = ['id', 'radius_mean', '']
 
 
 #Print o dataframe final e mostre a distribuição de classes que você deve classificar
