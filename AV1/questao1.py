751eb6bd096f10d455da21fd01506558351ea931
VictorMatheus <victormatheus@alu.ufc.br>
2023-04-19 16:50:05 -0300

    Merge branch 'main' of https://github.com/alysonbz/IA into Victor_Matheus # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit. projeto avaliação

--- AV1/questao1.py
+++ AV1/questao1.py
@@ -4,32 +4,20 @@ import pandas as pd
 ## Carregue o dataset definido para você
 df = pd.read_csv('customer_data.csv')
 
-print(df)
-
 # Verifique se existem celulas vazias ou Nan. Se existir, excluir e criar um novo dataframe.
-# verifica se há valores faltantes (NaN ou células vazias) no DataFrame
 if df.isna().any().any():
-    # exclui as linhas com valores faltantes
     df = df.dropna()
 
-# preenche os valores faltantes restantes com zero
-
-print(df)
-
 # Verifique quais colunas são as mais relevantes e crie um novo dataframe.
-
 
 
 #Print o dataframe final e mostre a distribuição de classes que você deve classificar
 counts = df['label'].value_counts()
 print('\nDistribuição de classes:\n', counts)
 
-
 #observe se a coluna de classes precisa ser renomeada para atributos numéricos, realize a conversão, se necessário
 #Não é necessário renomear a coluna "label" se ela já representa a classe do problema.
 
-
 #Salve o dataset atualizado se houver modificações.
-
 customer_ajustado = df
 customer_ajustado.to_csv = ('customer_ajustado.csv')
--- AV1/questao2.py
+++ AV1/questao2.py
@@ -5,33 +5,19 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import warnings
 warnings.filterwarnings("ignore", message="^In SciPy 1.11.0, the default value of `keepdims` will become False.*$")
-
-#Esse é apenas um aviso do Sklearn, indicando que o comportamento padrão da função modeserá alterado em uma versão futura do SciPy. No entanto, isso não afeta o funcionamento do seu código. O seu código rodou corretamente e a acurácia foi impressa no final. Portanto, você pode ignorar esse aviso por enquanto.
+#Esse é apenas um aviso do Sklearn, indicando que o comportamento padrão da função mode será alterado em uma versão futura do SciPy. No entanto, isso não afeta o funcionamento do código. O código rodou corretamente e a acurácia foi impressa no final. Portanto, pode ignorar esse aviso.
 
 # Carregue o conjunto de dados. Se houver um conjunto de dados atualizado, carregue o atualizado.
 customer_ajustado = customer_ajustado
-
-
-# Separa os atributos das classes.
 X = customer_ajustado.drop('label', axis=1)
 y = customer_ajustado['label']
 
 # Sem normalizar o conjunto de dados dividido o conjunto de dados em treino e teste.
-# Divide o conjunto de dados em treino e teste (70% treino, 30% teste).
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42, stratify=y)
-
-# Instancia o classificador KNN com k=5 (valor padrão).
 knn = KNeighborsClassifier()
-
-# Treina o classificador com o conjunto de treino.
 knn.fit(X_train, y_train)
 
 # Implementa o KNN exibindo a acurácia nos dados de teste mantendo a parametrização padrão.
-# Faz a predição no conjunto de teste.
 y_pred = knn.predict(X_test)
-
-# Calcula a acurácia da predição.
 accuracy = accuracy_score(y_test, y_pred)
-
-# Imprime a acurácia.
 print('Acurácia sem ser normalizada:', accuracy)
