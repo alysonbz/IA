df662be1b40b72dc4158eb49b8350b253626a792
Emilymendc <emilymendc@alu.ufc.br>
2023-04-20 17:00:13 -0300

    Oie, estou adicionando comentarios e finalizando minha att de casa:)

--- Pre-processing/Unidade2/Atividade_de_casa.py
+++ Pre-processing/Unidade2/Atividade_de_casa.py
@@ -181,3 +181,124 @@ print("Porcentagem de acertos:", 100 * acertos / len(teste))
 # Após a classificação, é feita uma comparação entre a classe atribuída pelo KNN e a classe real da amostra de teste (que está mantida
 # na última posição da lista da amostra). Se as duas classes forem iguais, é incrementada a contagem de acertos. Ao final do loop, a
 # precisão do algoritmo é continuada como o número de acertos dividido pelo número total de amostra de teste.
+
+# Retornando os dados
+iris = load_iris()
+iris
+
+# Transformando em um DataFrame
+dados_iris = pd.DataFrame(iris.data, columns = iris.feature_names)
+dados_iris['target'] = iris.target
+
+# Visualizando a base de dados
+
+# Selecionando a coluna pétala
+petala = dados_iris.loc[dados_iris.target.isin([1,2]),['petal length (cm)','petal width (cm)','target']]
+petala
+
+# Dividindo em X e y
+X = petala[['petal length (cm)','petal width (cm)']]
+y = petala.target
+
+# Dividindo em treino e teste
+X_train,X_test,y_train,y_test = train_test_split(X, y, test_size=0.2,random_state=42, stratify=y)
+
+# Formando os vizinhos
+neighbors = np.arange(1, 12)
+train_accuracies = {}
+test_accuracies = {}
+
+for neighbor in neighbors:
+    # Configurando um classificador KNN
+    knn = KNeighborsClassifier(n_neighbors=neighbor)
+
+    # Ajuste do modelo
+    knn.fit(X_train, y_train)
+
+    # Precisão de cálculo
+    train_accuracies[neighbor] = knn.score(X_train, y_train)
+    test_accuracies[neighbor] = knn.score(X_test, y_test)
+
+print("\nAcurácia (precisão) do treino: ",train_accuracies,"\nAcurácia (precisão) do teste: ", test_accuracies)
+
+# Visualizando os dados de treino
+fig, ax = plt.subplots()
+ax.scatter(x=X_train['petal width (cm)'],
+           y=X_train['petal length (cm)'],
+           c=y_train,
+           cmap='viridis')
+ax.set(xlim=(0.9, 2.6), xticks=[1,1.5,2,2.5],
+       ylim=(3, 7), yticks=[3,4,5,6,7])
+plt.title('Dados de treino')
+plt.show()
+
+# Quantidades de vizinhos
+clf = KNeighborsClassifier(n_neighbors=3)
+
+# fit com os dados de treino
+clf = clf.fit(X_train,y_train)
+
+# Previsão para os dados de teste
+y_pred = clf.predict(X_test)
+
+# Visualizando Treino e Teste
+fig, ax = plt.subplots()
+ax.scatter(x=X_train['petal width (cm)'],
+           y=X_train['petal length (cm)'],
+           c=y_train, alpha=0.7,
+           cmap='viridis')
+ax.scatter(x=X_test['petal width (cm)'],
+           y=X_test['petal length (cm)'],
+           c=y_pred,alpha=0.2,
+           cmap='RdYlGn')
+ax.scatter(x=X_test['petal width (cm)'],
+           y=X_test['petal length (cm)'],
+           c=y_test,alpha=0.2,
+           cmap='RdYlGn')
+ax.set(xlim=(0.9, 2.6), xticks=[1,1.5,2,2.5],
+       ylim=(3, 7), yticks=[3,4,5,6,7])
+plt.title('Dados de treino e teste')
+plt.show()
+
+X_test[y_test != y_pred]
+
+print("\n Distâncias Calculadas")
+
+# Euclidiana
+a = np.array(petala['petal length (cm)'])
+b = np.array(petala['petal width (cm)'])
+print("\nDistância Euclidiana: ")
+dist_euc = np.sqrt(np.sum(np.square(a-b)))
+print(dist_euc)
+
+# Manhattan
+# função para calcular
+def manhattan_distance(point1, point2):
+    return sum(abs(value1 - value2) for value1, value2 in zip(point1, point2))
+# Definindo dois pontos
+a = np.array(petala['petal length (cm)'])
+b = np.array(petala['petal width (cm)'])
+# resultado
+print("\nDistância Manhattan: ")
+print(manhattan_distance(a, b))
+
+# Minkowski
+# Definindo dois pontos
+a = np.array(petala['petal length (cm)'])
+b = np.array(petala['petal width (cm)'])
+# Calculando a distância Minkowski com ordem p=3
+p = 3
+dist_minkowski = np.linalg.norm(a - b, ord=p)
+# resultado
+print("\nDistância de Minkowski: ")
+print(dist_minkowski)
+
+# Chebyshev
+# Definindo dois pontos
+a = np.array(petala['petal length (cm)'])
+b = np.array(petala['petal width (cm)'])
+# Mostrando na tela
+print("\nDistância de Chebyshev: ")
+# Calculando a distância Chebyshev entre os dois pontos
+dist_chebyshev = np.amax(np.abs(a - b))
+print(dist_chebyshev)
