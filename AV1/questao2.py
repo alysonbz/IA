c06fb6808a61398ba439e63f41f7e25d3610b2ac
Eduardo <eteles352@gmail.com>
2023-04-20 04:33:15 -0300

    Final prova

--- AV1/questao2.py
+++ AV1/questao2.py
@@ -5,14 +5,12 @@ import pandas as pd
 
 # Carregue o dataset. Se houver o dataset atualizado, carregue o atualizado.
 df = pd.read_csv('Hotel_Reservations_ajustado.csv')
-#print(df)
 
 # Sem normalizar o conjunto de dados divida o dataset em treino e teste.
 
 X = df.drop('booking_status', axis=1)
 
 y = df['booking_status'].values
-
 
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, random_state=42)
@@ -23,4 +21,4 @@ knn = KNeighborsClassifier()
 
 knn.fit(X_train, y_train)
 
-print(knn.score(X_test, y_test))
+print("A acuracia de teste knn foi de: ",knn.score(X_test, y_test))
