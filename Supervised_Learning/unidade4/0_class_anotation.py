53bff4c59a32d28f8c34f5469d39d2490a4f0289
Larissa Vitória <larissavitory33@gmail.com>
2023-04-25 14:58:19 -0300

    Aula 24-04-23

--- Supervised_Learning/unidade4/0_class_anotation.py
+++ Supervised_Learning/unidade4/0_class_anotation.py
@@ -1,4 +1,38 @@
+import matplotlib.pyplot as plt
 from src.utils import load_diabetes_clean_dataset
+from sklearn.linear_model import LinearRegression
 
 diabetes_df = load_diabetes_clean_dataset()
 
+# Tratando valores nulos
+#diabetes_df = diabetes_df.loc[diabetes_df['bmi'] != 0]
+
+print(diabetes_df.head(5))
+
+# Separando o objeto target
+X = diabetes_df.drop("glucose", axis = 1).values
+y = diabetes_df["glucose"].values
+print(type(X), type(y))
+
+# Realizando uma regressão utilizando 1 único atributo
+X_bmi = X[:, 3]
+print(y.shape, X_bmi.shape)
+
+X_bmi = X_bmi.reshape(-1, 1)
+print(X_bmi.shape)
+
+# Plotando o gráfico da glicose em função do IMC
+plt.scatter(X_bmi, y)
+plt.ylabel("Glicose no sangue (mg/dl)")
+plt.xlabel("Índice de massa corporal")
+plt.show()
+
+# Treinando um modelo de regressão
+reg = LinearRegression()
+reg.fit(X_bmi, y)
+predictions = reg.predict(X_bmi)
+plt.scatter(X_bmi, y)
+plt.plot(X_bmi, predictions)
+plt.ylabel("Glicose no sangue (mg/dl)")
+plt.xlabel("Índice de massa corporal")
+plt.show()
--- Supervised_Learning/unidade4/1_create_features.py
+++ Supervised_Learning/unidade4/1_create_features.py
@@ -2,15 +2,16 @@ import numpy as np
 from src.utils import load_sales_clean_dataset
 
 sales_df = load_sales_clean_dataset()
+print(sales_df)
 
 # Create X from the radio column's values
-X = ____
+X = sales_df["radio"].values
 
 # Create y from the sales column's values
-y = ____
+y = sales_df["sales"].values
 
 # Reshape X
-X = ____
+X_sales = X.reshape(-1, 1)
 
 # Check the shape of the features and targets
-print(____)
+print(X_sales.shape, y.shape)
