4531ccc5ab8c2422bb458621e1581e8663477e22
alekyne <aryanialekyni@gmail.com>
2023-04-25 14:56:42 -0300

    Finalizado

--- Supervised_Learning/unidade4/0_class_anotation.py
+++ Supervised_Learning/unidade4/0_class_anotation.py
@@ -1,4 +1,37 @@
+import matplotlib.pyplot as plt
+
 from src.utils import load_diabetes_clean_dataset
 
 diabetes_df = load_diabetes_clean_dataset()
+
+print(diabetes_df.head())
+
+X = diabetes_df.drop('glucose', axis = 1).values
+y = diabetes_df['glucose'].values
+print(type(X), type(y))
+
+
+X_bmi = X[:, 3]
+print(y.shape, X_bmi.shape)
+
+
+X_bmi = X_bmi.reshape(-1, 1)
+print(X_bmi.shape)
+
+import matplotlib.pyplot as plt
+
+plt.scatter(X_bmi, y)
+plt.ylabel('Blood Glucose (mg/dl)')
+plt.xlabel('Body mass Index')
+plt.show()
+
+from sklearn.linear_model import LinearRegression
+reg = LinearRegression()
+reg.fit(X_bmi, y)
+predictions = reg.predict(X_bmi)
+plt.scatter(X_bmi, y)
+plt.plot(X_bmi, predictions)
+plt.ylabel('Blood Glucose (Mg/dl)')
+plt.xlabel('Body mass Index')
+plt.show()
 
