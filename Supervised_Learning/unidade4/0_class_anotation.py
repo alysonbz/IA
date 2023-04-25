b820750fc1e6a463667e32a1d151e157ed1b66ee
henriquesantos769 <henripa224444@gmail.com>
2023-04-25 14:56:05 -0300

    Atividades, prova/projeto.

--- Supervised_Learning/unidade4/0_class_anotation.py
+++ Supervised_Learning/unidade4/0_class_anotation.py
@@ -1,4 +1,32 @@
 from src.utils import load_diabetes_clean_dataset
+import matplotlib.pyplot as plt
+from sklearn.linear_model import  LinearRegression
+
 
 diabetes_df = load_diabetes_clean_dataset()
+print(diabetes_df.head(5))
+
+x = diabetes_df.drop("glucose", axis = 1).values
+y = diabetes_df["glucose"].values
+print(type(x), type(y))
+
+x_bmi = x[:, 3]
+print(y.shape, x_bmi.shape)
+x_bmi = x_bmi.reshape(-1,  1)
+print(x_bmi.shape)
+
+plt.scatter(x_bmi, y)
+plt.ylabel('Alguma coias')
+plt.xlabel('algo')
+plt.show()
+
+#Plotanto a linha
+reg = LinearRegression()
+reg.fit(x_bmi, y)
+predictions =  reg.predict(x_bmi)
+plt.scatter(x_bmi, y)
+plt.plot(x_bmi, predictions)
+plt.ylabel("Blood Glucose (mg/dl)")
+plt.xlabel("Body Rass  index")
+plt.show()
 
--- Supervised_Learning/unidade4/1_create_features.py
+++ Supervised_Learning/unidade4/1_create_features.py
@@ -4,13 +4,13 @@ from src.utils import load_sales_clean_dataset
 sales_df = load_sales_clean_dataset()
 
 # Create X from the radio column's values
-X = ____
+x = sales_df['radio'].values
 
 # Create y from the sales column's values
-y = ____
+y = sales_df['sales'].values
 
 # Reshape X
-X = ____
+X = x.reshape(-1, 1)
 
 # Check the shape of the features and targets
-print(____)
+print(x.shape, y.shape)
