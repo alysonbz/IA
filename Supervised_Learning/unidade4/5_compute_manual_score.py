5373e008d7d56d235cdc858efc3572c0c643c365
Matheus Rabelo <matheusrabelo@alu.ufc.br>
2023-05-03 14:59:03 -0300

    Unidade 4, Materia: IA Cross Validation

--- Supervised_Learning/unidade4/5_compute_manual_score.py
+++ Supervised_Learning/unidade4/5_compute_manual_score.py
@@ -9,10 +9,13 @@ def compute_MSE(predictions,y):
     MSE= np.sum(np.square(y-predictions))/len(predictions)
     return MSE
 def compute_RMSE(predictions,y):
-    RMSE = None
+    MSE = compute_MSE(predictions, y)
+    RMSE = np.sqrt(MSE)
     return RMSE
 def compute_R_squared(predictions,y):
-    r_squared = None
+    var_pred = np.sum(np.square(predictions - np.mean(y)))
+    var_data = np.sum(np.square(y-np.mean(y)))
+    r_squared = np.divide(var_pred, var_data)
     return r_squared
 
 
