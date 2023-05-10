d2b8a87be883912f05fde45e16959a1303e455a7
Luis_Savio <luissavio150@gmail.com>
2023-05-10 17:11:53 -0300

    Regressão linear

--- Supervised_Learning/unidade4/5_compute_manual_score.py
+++ Supervised_Learning/unidade4/5_compute_manual_score.py
@@ -2,21 +2,21 @@ import numpy as np
 from src.utils import processing_all_features_sales_clean
 
 def compute_RSS(predictions,y):
-    RSS = None
+    RSS = np.sum(np.square(y - predictions))
     return RSS
 def compute_MSE(predictions,y):
-    MSE= None
+    MSE = (np.square(y - predictions)/len(predictions))
     return MSE
+
 def compute_RMSE(predictions,y):
-    RMSE = None
+    RMSE = np.sqrt(np.mean(np.square(y - predictions)))
     return RMSE
+
 def compute_R_squared(predictions,y):
     r_squared = None
     return r_squared
 
-
 X,y,predictions = processing_all_features_sales_clean()
-
 
 print("RSS: {}".format(compute_RSS(predictions,y)))
 print("MSE: {}".format(compute_MSE(predictions,y)))
--- Supervised_Learning/unidade4/8_lasso_regression.py
+++ Supervised_Learning/unidade4/8_lasso_regression.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 from src.utils import load_sales_clean_dataset
 
 # Import Lasso
-from ____.____ import ____
+from sklearn.linear_model import Lasso
 
 sales_df = load_sales_clean_dataset()
 
@@ -12,10 +12,10 @@ y = sales_df["sales"].values
 sales_columns = X.columns
 
 # Instantiate a lasso regression model
-lasso = ____
+lasso = Lasso(alpha=0.3)
 
 # Compute and print the coefficients
-lasso_coef = ____
+lasso_coef = lasso.fit(X,y).coef_
 print(lasso_coef)
 plt.bar(sales_columns, lasso_coef)
 plt.xticks(rotation=45)
