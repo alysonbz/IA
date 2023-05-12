d8615966cc06c18671505643c8ed823f32f9adae
Emilymendc <emilymendc@alu.ufc.br>
2023-05-11 23:13:34 -0300

    Atualizando aulas passadas de IA! :)

--- Supervised_Learning/unidade4/5_compute_manual_score.py
+++ Supervised_Learning/unidade4/5_compute_manual_score.py
@@ -2,16 +2,20 @@ import numpy as np
 from src.utils import processing_all_features_sales_clean
 
 def compute_RSS(predictions,y):
-    RSS = None
+    RSS = np.sum(np.square(y - predictions))
     return RSS
+
 def compute_MSE(predictions,y):
-    MSE= None
+    MSE= np.sum(np.square(y-predictions))/len(predictions)
     return MSE
 def compute_RMSE(predictions,y):
-    RMSE = None
+    MSE = compute_MSE(predictions, y)
+    RMSE = np.sqrt(MSE)
     return RMSE
 def compute_R_squared(predictions,y):
-    r_squared = None
+    var_pred = np.sum(np.square(predictions - np.mean(y)))
+    var_data = np.sum(np.square(y-np.mean(y)))
+    r_squared = np.divide(var_pred, var_data)
     return r_squared
 
 
