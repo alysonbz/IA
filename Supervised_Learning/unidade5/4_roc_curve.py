f957a0ea03c83e06785ec4132f3d9fe555f4fce6
Larissa Vitória <larissavitory33@gmail.com>
2023-05-16 15:10:29 -0300

    Aula 16-05-23, curva ROC

--- Supervised_Learning/unidade5/4_roc_curve.py
+++ Supervised_Learning/unidade5/4_roc_curve.py
@@ -1,17 +1,17 @@
 import matplotlib.pyplot as plt
 from src.utils import log_reg_diabetes
 # Import roc_curve
-____
+from sklearn.metrics import roc_curve
 
 y_prob,y_test ,_= log_reg_diabetes()
 
 # Generate ROC curve values: fpr, tpr, thresholds
-fpr, tpr, thresholds = ____(____, ____)
+fpr, tpr, thresholds = roc_curve(y_test, y_prob)
 
 plt.plot([0, 1], [0, 1], 'k--')
 
 # Plot tpr against fpr
-plt.plot(____, ____)
+plt.plot(fpr, tpr)
 plt.xlabel('False Positive Rate')
 plt.ylabel('True Positive Rate')
 plt.title('ROC Curve for Diabetes Prediction')
