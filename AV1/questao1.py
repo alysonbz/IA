270dbd7dcfe34ea78304cd20abb5377b176ca1a8
LAB1_14 <williamkelvin@alu.ufc.br>
2023-05-16 16:55:02 -0300

    aula de KNN

--- AV1/questao1.py
+++ AV1/questao1.py
@@ -1,24 +1,25 @@
 #importe as bibliotecas necessárias
-
+import pandas as pd
+import numpy as np
 
 ## Carregue o dataset definido para você
+df = pd.read_csv(r"C:\Users\LAB1_00\Desktop\kelvincbl\IA\AV1\dataset\waterQuality1.csv")
+print(df.info())
 
-
-
-# Verifique se existem celulas vazias ou Nan. Se existir, excluir e criar um novo dataframe.
-
-
+# Verifique se existem colunas vazias ou Nan. Se existir, excluir e criar um novo dataframe.
+print(df.isna().sum())
 
 # Verifique quais colunas são as mais relevantes e crie um novo dataframe.
-
+df = df['']
 
 
 #Print o dataframe final e mostre a distribuição de classes que você deve classificar
-
+print(df['Potability'].value_counts())
 
 
 #observe se a coluna de classes precisa ser renomeada para atributos numéricos, realize a conversão, se necessário
-
+df.rename(columns={'Potability': 'Class'}, inplace=True)
 
 
 #Salve o dataset atualizado se houver modificações.
+df.to_csv('water_quality_ajustado.csv', index=False)
--- Supervised_Learning/unidade4/1_create_features.py
+++ Supervised_Learning/unidade4/1_create_features.py
@@ -4,10 +4,10 @@ from src.utils import load_sales_clean_dataset
 sales_df = load_sales_clean_dataset()
 
 # Create X from the radio column's values
-X = ____
+X =
 
 # Create y from the sales column's values
-y = ____
+y = _
 
 # Reshape X
 X = ____
