5ccaa98da24ad97419430e9b71394e4ac270b200
Larissa Vitória <larissavitory33@gmail.com>
2023-05-18 15:54:44 -0300

    Aula 17/05/2023

--- Supervised_Learning/unidade5/6_hyperparam_tuning_grid_search.py
+++ Supervised_Learning/unidade5/6_hyperparam_tuning_grid_search.py
@@ -1,16 +1,16 @@
 import numpy as np
 
 #import Lasso
-_______
+from sklearn.linear_model import Lasso
 
 #import train_test_split
 from sklearn.model_selection import train_test_split
 
 #import kfold
-____
+from sklearn.model_selection import KFold
 
 # Import GridSearchCV
-______
+from sklearn.model_selection import GridSearchCV
 
 from src.utils import load_diabetes_clean_dataset
 
@@ -21,20 +21,19 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 
 
 #inicialize Lasso
-lasso  = ____
+lasso = Lasso()
 
 #inicialize kfold
-kf = _____
+kf = KFold(n_splits=5, shuffle=True, random_state=42)
 
 #Set up the parameter grid
-param_grid = {"____": np.linspace(____, __, ___)}
+param_grid = {"alpha": np.linspace(0.00001, 1, 10)}
 
 # Instantiate lasso_cv
-lasso_cv = ____
+lasso_cv = GridSearchCV(lasso, param_grid, cv=kf)
 
 # Fit to the training data
-___
-
+lasso_cv.fit(X_train, y_train)
 
 print("Tuned lasso paramaters: {}".format(lasso_cv.best_params_))
 print("Tuned lasso score: {}".format(lasso_cv.best_score_))
