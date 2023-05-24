d4bb8b4488dfb38b18afff7c6c86344213b74b36
Eryka <129187562+Erykash@users.noreply.github.com>
2023-05-24 19:52:15 -0300

    help God

--- Supervised_Learning/unidade5/5_auc.py
+++ Supervised_Learning/unidade5/5_auc.py
@@ -1,5 +1,6 @@
 from src.utils import log_reg_diabetes
 from sklearn.metrics import classification_report, confusion_matrix
+
 # Import roc_auc_score
 from sklearn.metrics import roc_auc_score
 
@@ -13,3 +14,20 @@ print(confusion_matrix(y_test, y_pred))
 
 # Calculate the classification report
 print(classification_report(y_test, y_prob))
+
+
+#KNN
+
+from src.utils import load_churn_dataset
+
+from sklearn.neighbors import KNeighborsClassifier
+
+diabetes_df = log_reg_diabetes
+
+y = diabetes_df["diabetes"].values
+X = diabetes_df[["insulin", "glucose"]].values
+
+knn = KNeighborsClassifier(n_neighbors=5)
+
+knn.fit(X, y)
+
