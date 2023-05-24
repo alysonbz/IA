998f7f7c074bb3c3409da3ee811545299f7c8135
Emily Mendonça <emilymendc@alu.ufc.br>
2023-05-24 14:59:47 -0300

    Atualizando atividades de casa e iniando a AV2.

--- AV2/questao1.py
+++ AV2/questao1.py
@@ -1,13 +1,19 @@
 #importe as bibliotecas necessárias
-
+import pandas as pd
 
 ## Carregue o dataset definido para você
-
+smarthwatch = pd.read_csv('Smartwatchprices.csv')
+print("\n Dataset: Smart watch prices")
+print(smarthwatch)
 
 
 # Verifique se existem celulas vazias ou Nan. Se existir, excluir e criar um novo dataframe.
+print("\n Verificação da existência de células vaizas ou Nan")
+print(smarthwatch.isna().sum())
 
-
+print("\n Dropando Células Vazias")
+smarthwatch_sem_na = smarthwatch.dropna()
+print(smarthwatch_sem_na.isna().sum())
 
 # Verifique quais colunas são as mais relevantes e crie um novo dataframe.
 
--- Supervised_Learning/unidade5/2_compute_metric_manual.py
+++ Supervised_Learning/unidade5/2_compute_metric_manual.py
@@ -1,37 +1,109 @@
 from src.utils import process_diabetes
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
-
-def compute_acuraccy(y_pred ,y_test):
-    return None
-
-def compute_recall(y_pred , y_test):
-    return None
-
-def compute_precision(y_pred, y_test):
-    return None
-
-def compute_f1_score(y_pred,y_test):
-    return None
-
-def compute_confusion_matriz(y_pred,y_test):
-    return None
-
-y_pred ,y_test = process_diabetes()
+import numpy as np
 
 
+class Metrics:
 
-print("acurácia geral:")
+    def __init__(self, y_pred, y_test):
+        self.vp_c1 = 0
+        self.vn_c1 = 0
+        self.fp_c1 = 0
+        self.fn_c1 = 0
+        self.vp_c0 = 0
+        self.vn_c0 = 0
+        self.fp_c0 = 0
+        self.fn_c0 = 0
+
+    def set_param_classe1(self):
+
+        for yp, yt in zip(y_pred, y_test):
+            if yp == 1 and yt == 1:
+                self.vp_c1 = self.vp_c1 + 1
+            if yp == 1 and yt == 0:
+                self.fp_c1 = self.fp_c1 + 1
+            if yp == 0 and yt == 1:
+                self.fn_c1 = self.fn_c1 + 1
+            if yp == 0 and yt == 0:
+                self.vn_c1 = self.vn_c1 + 1
+
+    def set_param_classe2(self):
+
+        for yp, yt in zip(y_pred, y_test):
+            if yp == 0 and yt == 0:
+                self.vp_c0 = self.vp_c0 + 1
+            if yp == 0 and yt == 1:
+                self.fp_c0 = self.fp_c0 + 1
+            if yp == 1 and yt == 0:
+                self.fn_c0 = self.fn_c0 + 1
+            if yp == 1 and yt == 1:
+                self.vn_c0 = self.vn_c0 + 1
+
+
+    def compute_acuraccy(self):
+        assert len(y_pred) == len(y_test), "Verifica se o número de predições é igual ao número de rótulos verdadeiros."
+        num_acertos = np.sum(y_pred == y_test)
+        total_amostras = len(y_pred)
+        acuracia = (num_acertos / total_amostras) * 100
+        return acuracia
+
+    def compute_recall_c1(self):
+        recall_c1 = np.sum(self.vp_c1) / (np.sum(self.vp_c1 ) + np.sum(self.fn_c1))
+        return recall_c1
+
+    def compute_recall_c0(self):
+        recall_c0 = np.sum(self.vp_c0) / (np.sum(self.vp_c0) + np.sum(self.fn_c0))
+        return recall_c0
+
+    def compute_precision_c1(self):
+        precision_c1 = np.sum(self.vp_c1) / (np.sum(self.vp_c1) + np.sum(self.fp_c1))
+        return precision_c1
+
+    def compute_precision_c0(self):
+        precision_c0 = np.sum(self.vp_c0)/ (np.sum(self.vp_c0) + np.sum(self.fp_c0))
+        return precision_c0
+
+    def compute_f1_score_c1(self):
+        recall_c1 = np.sum(self.vp_c1) / (np.sum(self.vp_c1) + np.sum(self.fn_c1))
+        precision_c1 = np.sum(self.vp_c1) / (np.sum(self.vp_c1) + np.sum(self.fp_c1))
+        if precision_c1 > 0 or recall_c1 > 0:
+            f1_score_c1 = 2 * (precision_c1 * recall_c1) / (precision_c1 + recall_c1)
+        else:
+            f1_score_c1 = 0
+        return f1_score_c1
+    def compute_f1_score_c0(self):
+        recall_c0 = np.sum(self.vp_c0) / (np.sum(self.vp_c0) + np.sum(self.fn_c0))
+        precision_c0 = np.sum(self.vp_c0)/ (np.sum(self.vp_c0) + np.sum(self.fp_c0))
+        if precision_c0 > 0 or recall_c0 > 0:
+            f1_score_c0 = 2 * (precision_c0 * recall_c0) / (precision_c0 + recall_c0)
+        else:
+            f1_score_c0 = 0
+        return f1_score_c0
+
+    def compute_confusion_matriz(self):
+        confusion_matrix = [[self.vp_c0, self.fp_c0],
+                            [self.fp_c1, self.vp_c1]]
+        return confusion_matrix
+
+
+y_pred, y_test = process_diabetes()
+mt = Metrics(y_pred, y_test)
+mt.set_param_classe1()
+mt.set_param_classe2()
+
+print("acurácia geral:", mt.compute_acuraccy())
 #
-print("recall classe 0: ")
+print("recall classe 0: ", mt.compute_recall_c0())
 #
-print("recall classe 1: ")
+print("recall classe 1: ", mt.compute_recall_c1())
 #
-print("precision classe 0: ")
+print("precision classe 0: ", mt.compute_precision_c0())
 #
-print("precision classe 1:")
+print("precision classe 1:", mt.compute_precision_c1())
 #
-print("F1-score classe 0:")
+print("F1-score classe 0:", mt.compute_f1_score_c0())
 #
-print("F1-score classe 1:")
+print("F1-score classe 1:", mt.compute_f1_score_c1())
 #
+print("Matriz de confusão", mt.compute_confusion_matriz())
