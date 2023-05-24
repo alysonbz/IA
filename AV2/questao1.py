77ef67964b198356391ae2e252862ac606301f34
eduardw07 <eteles352@gmail.com>
2023-05-24 18:00:48 -0300

    Merge branch 'main' of https://github.com/alysonbz/IA into Eduardo_Teles
     Conflicts:
    	Supervised_Learning/unidade4/0_class_anotation.py

--- AV2/questao1.py
+++ AV2/questao1.py
@@ -1,12 +1,12 @@
 #importe as bibliotecas necessárias
-
+import pandas as pd
 
 ## Carregue o dataset definido para você
-
-
+df = pd.read_csv(r'C:\Users\LAB1_00\Desktop\Dataframe\HP_share_prices.csv')
+print(df)
 
 # Verifique se existem celulas vazias ou Nan. Se existir, excluir e criar um novo dataframe.
-
+print(df.isnull().sum())
 
 
 # Verifique quais colunas são as mais relevantes e crie um novo dataframe.
