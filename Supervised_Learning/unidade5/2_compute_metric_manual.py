f46cff2f454b552ef141f8677c169c28c2dfb109
Robertim <robertbarreira@alu.ufc.br>
2023-05-24 14:01:41 -0300

    concluido essas incriveis atividades

--- Supervised_Learning/unidade5/2_compute_metric_manual.py
+++ Supervised_Learning/unidade5/2_compute_metric_manual.py
@@ -1,3 +1,4 @@
+
 from src.utils import process_diabetes
 
 
--- Supervised_Learning/unidade5/3_logistic_regression.py
+++ Supervised_Learning/unidade5/3_logistic_regression.py
@@ -1,22 +1,21 @@
 from src.utils import load_diabetes_clean_dataset
 from sklearn.model_selection import train_test_split
 #Import LogisticRegression
-____
-
+from sklearn.linear_model import LogisticRegression
 
 diabetes_df = load_diabetes_clean_dataset()
 X = diabetes_df.drop(['diabetes'],axis=1)
 y = diabetes_df['diabetes'].values
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42,stratify=y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
 
 # Instantiate the model
-logreg = ____
+logreg = LogisticRegression()
 
 # Fit the model
-____
+logreg.fit(X_train, y_train)
 
 # Predict probabilities
-y_pred_probs = logreg.____(____)[____, ____]
+y_pred_probs = logreg.predict_proba(X_test)[:, 1]
 
 print(y_pred_probs[:10])
