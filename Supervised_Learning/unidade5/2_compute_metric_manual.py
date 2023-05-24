ebfca3c6d2906729f530ccb6c486cf604890b72d
Bia14k <maria.bianca.mb0@gmail.com>
2023-05-24 15:43:23 -0300

    Merge branch 'main' of https://github.com/alysonbz/IA into Bianca
     Conflicts:
    	Supervised_Learning/unidade4/0_class_anotation.py

--- Supervised_Learning/unidade5/2_compute_metric_manual.py
+++ Supervised_Learning/unidade5/2_compute_metric_manual.py
@@ -26,41 +26,45 @@ class Metrics:
                 self.vn_c1 = self.vn_c1 + 1
 
     def set_param_classe2(self):
-        pass
-        for yp, yt in zip(y_pred, y_test):
-        if yp == 0 and yt == 0:
-            self.vp_c0 = self.vp_c0 + 1
-        if yp == 0 and yt == 1:
-            self.fp_c0 = self.fp_c0 + 1
-        if yp == 1 and yt == 0:
-            self.fn_c0 = self.fn_c0 + 1
-        if yp == 1 and yt == 1:
-            self.vn_c0 = self.vn_c0 + 1
+        total_samples = len(self.y_pred)
+        correct_predictions = self.vp_c0 + self.vn_c0 + self.vp_c1 + self.vn_c1
+        accuracy = correct_predictions / total_samples
+        return accuracy
 
     def compute_acuraccy(self):
-        return None
+        recall_c1 = self.vp_c1 / (self.vp_c1 + self.fn_c1)
+        return recall_c1
 
     def compute_recall_c1(self):
-        return None
+        recall_c0 = self.vp_c0 / (self.vp_c0 + self.fn_c0)
+        return recall_c0
 
     def compute_recall_c0(self):
-        return None
+        recall_c0 = self.vp_c0 / (self.vp_c0 + self.fn_c0)
+        return recall_c0
 
     def compute_precision_c1(self):
-        return None
+        precision_c1 = self.vp_c1 / (self.vp_c1 + self.fp_c1)
+        return precision_c1
 
     def compute_precision_c0(self):
-        return None
+        precision_c0 = self.vp_c0 / (self.vp_c0 + self.fp_c0)
+        return precision_c0
 
     def compute_f1_score_c1(self):
-        return None
+        recall = self.compute_recall_c1()
+        precision = self.compute_precision_c1()
+        f1_score_c1 = 2 * (precision * recall) / (precision + recall)
+        return f1_score_c1
 
     def compute_f1_score_c0(self):
-        return None
+        recall = self.compute_recall_c0()
+        precision = self.compute_precision_c0()
+        f1_score_c0 = 2 * (precision * recall) / (precision + recall)
+        return f1_score_c0
 
     def compute_confusion_matriz(self):
-        return None
-
+        confusion_matrix = [[self.vp_c0, self.fp_c0], [self.fn_c0, self.vn_c0]], [[self.vp_c1, self.fp_c1],[self.fn_c1, self.vn_c1]]
 
 y_pred, y_test = process_diabetes()
 mt = Metrics(y_pred, y_test)
@@ -68,18 +72,17 @@ mt.set_param_classe1()
 mt.set_param_classe2()
 
 print("acurácia geral:", mt.compute_acuraccy())
-#
-print("recall classe 0: ", )
-#
-print("recall classe 1: ")
-#
-print("precision classe 0: ")
-#
-print("precision classe 1:")
-#
-print("F1-score classe 0:")
-#
-print("F1-score classe 1:")
-#
-print("Matriz de confusão")
-#
+    #
+print("recall classe 0: ", mt.compute_recall_c0)
+    #
+print("recall classe 1: ", mt.compute_recall_c1())
+    #
+print("precision classe 0: ", mt.compute_precision_c0())
+    #
+print("precision classe 1: ", mt.compute_precision_c1())
+    #
+print("F1-score classe 0: ", mt.compute_f1_score_c0())
+    #
+print("F1-score classe 1: ", mt.compute_f1_score_c1())
+    #
+print("Matriz de confusão", mt.compute_confusion_matriz())
