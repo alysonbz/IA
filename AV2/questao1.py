fe8c46f5e74d53251c3943b6cd475af4b082adf3
Lucas do Nascimento <lucasnascimentosousa@alu.ufc.br>
2023-05-25 14:45:21 -0300

    ap2- Q1

--- AV2/questao1.py
+++ AV2/questao1.py
@@ -1,35 +1,49 @@
 #importe as bibliotecas necessárias
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.linear_model import Lasso
 
 ## Carregue o dataset definido para você
 dados = pd.read_csv('laptopPrice.csv')
 
 
-# Verifique se existem celulas vazias ou Nan. Se existir, excluir e criar um novo dataframe.
-'''print(dados.isnull().sum())
-print(dados.isna().sum())'''
-
-
 
 # Verifique quais colunas são as mais relevantes e crie um novo dataframe.
-dm = dados[['brand', 'processor_brand', 'processor_name', 'processor_gnrtn', 'ram_gb', 'ram_type', 'ssd', 'Price', 'rating', 'Number of Ratings', 'Number of Reviews']]
+dm = pd.DataFrame(dados[['brand', 'processor_brand', 'processor_name', 'processor_gnrtn', 'ram_gb', 'ram_type', 'ssd', 'Price', 'rating', 'Number of Ratings', 'Number of Reviews']])
 
-
-#Print o dataframe final e mostre a distribuição de classes que você deve classificar
-print(dm['brand'].value_counts(),'\n','\n')
-print(dm['processor_brand'].value_counts(),'\n','\n')
-print(dm['processor_name'].value_counts(),'\n','\n')
-print(dm['processor_gnrtn'].value_counts(),'\n','\n')
-print(dm['ram_gb'].value_counts(),'\n','\n')
-print(dm['ram_type'].value_counts(),'\n','\n')
-print(dm['ssd'].value_counts(),'\n','\n')
-print(dm['Price'].value_counts(),'\n','\n')
-print(dm['rating'].value_counts(),'\n','\n')
-print(dm['Number of Ratings'].value_counts(),'\n','\n')
-print(dm['Number of Reviews'].value_counts(),'\n','\n')
-
-#observe se a coluna de classes precisa ser renomeada para atributos numéricos, realize a conversão, se necessário
+'''#conversão de strting para int
+dm['brand'].replace(['ASUS', 'Lenovo','acer', 'Avita', 'HP', 'DELL', 'MSI', 'APPLE'],
+                        [0,1], inplace=True)'''
 
 
 
-#Salve o dataset atualizado se houver modificações.
+# Converter a coluna "brand" de volta para valores string
+dados['brand', 'processor_name', 'processor_gnrtn', 'ram_gb', 'ram_type', 'ssd', 'Price', 'rating', 'Number of Ratings', 'Number of Reviews'].astype(str)
+
+# Exibir o DataFrame com a coluna "brand" convertida para string
+print(dados)
+
+'''#atributo mais relevante
+X = dm.drop(['Price'], axis=1)
+y = dados['Price'].values
+dm_columns = X.columns
+'''
+
+
+
+
+
+
+'''# Instantiate a lasso regression model
+lasso = Lasso(alpha=0.1)
+
+# Compute and print the coefficients
+lasso_coef = lasso.fit(X, y).coef_
+print(lasso_coef)
+plt.bar(dm_columns, lasso_coef)
+plt.xticks(rotation=45)
+plt.show()'''
