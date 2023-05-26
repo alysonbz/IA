eb51e9bbee15e78063f31af49cb9f8ec5811a923
MASTER <aryaanialekyni@gmail.com>
2023-05-25 23:19:13 -0300

    AP2  25/05/2023

--- AV2/questao1.py
+++ AV2/questao1.py
@@ -1,49 +1,47 @@
 #importe as bibliotecas necessárias
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
-import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.linear_model import Lasso
+from sklearn.preprocessing import LabelEncoder
+
 
 ## Carregue o dataset definido para você
 dados = pd.read_csv('laptopPrice.csv')
 
 
 
-# Verifique quais colunas são as mais relevantes e crie um novo dataframe.
-dm = pd.DataFrame(dados[['brand', 'processor_brand', 'processor_name', 'processor_gnrtn', 'ram_gb', 'ram_type', 'ssd', 'Price', 'rating', 'Number of Ratings', 'Number of Reviews']])
+# Verifique quais colunas são as mais relevantes e crie um novo dataframe
+colunas_relevantes = ['brand', 'processor_brand', 'processor_name', 'processor_gnrtn', 'ram_gb', 'ram_type', 'ssd', 'Price', 'rating', 'Number of Ratings', 'Number of Reviews']
+dm = dados[colunas_relevantes].copy()
 
-'''#conversão de strting para int
-dm['brand'].replace(['ASUS', 'Lenovo','acer', 'Avita', 'HP', 'DELL', 'MSI', 'APPLE'],
-                        [0,1], inplace=True)'''
+# Criar uma instância do LabelEncoder
+label_encoder = LabelEncoder()
+
+# Aplicar o LabelEncoder às colunas categóricas
+for coluna in colunas_relevantes:
+    if dm[coluna].dtype == 'object':
+        dm.loc[:, coluna] = label_encoder.fit_transform(dm[coluna])
+
+# Exibir o DataFrame com as colunas codificadas
+print(dm)
 
 
-
-# Converter a coluna "brand" de volta para valores string
-dados['brand', 'processor_name', 'processor_gnrtn', 'ram_gb', 'ram_type', 'ssd', 'Price', 'rating', 'Number of Ratings', 'Number of Reviews'].astype(str)
-
-# Exibir o DataFrame com a coluna "brand" convertida para string
-print(dados)
-
-'''#atributo mais relevante
+#atributo mais relevante
 X = dm.drop(['Price'], axis=1)
-y = dados['Price'].values
+y = dm['Price'].values
 dm_columns = X.columns
-'''
 
 
 
-
-
-
-'''# Instantiate a lasso regression model
-lasso = Lasso(alpha=0.1)
+# Instantiate a lasso regression model
+lasso = Lasso(alpha=0.3)
 
 # Compute and print the coefficients
 lasso_coef = lasso.fit(X, y).coef_
 print(lasso_coef)
 plt.bar(dm_columns, lasso_coef)
 plt.xticks(rotation=45)
-plt.show()'''
+plt.show()
+
+
+dm.to_csv("dados.csv", index=False)
