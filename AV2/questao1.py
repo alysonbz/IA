bd06247a0b732df2b4e813c51df4417ce3fe3d19
ruan rodrigues <ruanrodrigues9ufc@gmail.com>
2023-05-27 00:16:31 -0300

    Merge branch 'ruan_rodrigues' of https://github.com/alysonbz/IA progress.py test

--- AV2/questao1.py
+++ AV2/questao1.py
@@ -10,20 +10,20 @@ from sklearn.preprocessing import LabelEncoder
 
 
 ## Carregue o dataset definido para você
-bodyfat= pd.read_csv(r"C:\Users\ruanr\Downloads\archive (1)\bodyfat.csv")
-bodyfat.info()
+df= pd.read_csv(r"C:\Users\ruanr\Downloads\archive (1)\bodyfat.csv")
+df.info()
 
 
 
 # Verifique se existem celulas vazias ou Nan. Se existir, excluir e criar um novo dataframe.
 print("\n Verificação da existência de células vaizas ou Nan")
-print(bodyfat.isna().sum())
+print(df.isna().sum())
 
 
 
 # Verifique quais colunas são as mais relevantes e crie um novo dataframe.
 print("\n colunas relevantes")
-bodyfat_ajust = bodyfat[['Density',
+df_final = df[['Density',
                           'BodyFat',
                           'Age',
                           'Weight',
@@ -31,35 +31,49 @@ bodyfat_ajust = bodyfat[['Density',
 
 
 #Print o dataframe final e mostre a distribuição de classes que você deve classificar
-print(bodyfat_ajust)
-
-
-#observe se a coluna de classes precisa ser renomeada para atributos numéricos, realize a conversão, se necessário
-# Criar uma instância do LabelEncoder
-label_encoder = LabelEncoder()
-# Aplicar o LabelEncoder às colunas categóricas
-for coluna in bodyfat_ajust:
-    if bodyfat_ajust[coluna].dtype == 'object':
-        bodyfat_ajust.loc[:, coluna] = label_encoder.fit_transform(bodyfat_ajust[coluna])
-#atributo mais relevante
-X = bodyfat_ajust.drop(['BodyFat'], axis=1)
-y = bodyfat_ajust['BodyFat'].values
-y = bodyfat_ajust['BodyFat'].values
-bodyfat_ajust_columns = X.columns
-''# Instantiate a lasso regression model
-lasso = Lasso(alpha=0.1)
+print(df_final)
+# Create X and y arrays
+X = df_final.drop(["BodyFat"], axis=1).select_dtypes(exclude=["object"])
+y = df_final["BodyFat"].values
+sales_columns = X.columns
 # Instantiate a lasso regression model
 lasso = Lasso(alpha=0.3)
 
 # Compute and print the coefficients
-lasso_coef = lasso.fit(X, y).coef_
+lasso_coef = lasso.fit(X,y).coef_
 print(lasso_coef)
-plt.bar(bodyfat_ajust_columns, lasso_coef)
+plt.bar(sales_columns, lasso_coef)
+
 plt.xticks(rotation=45)
 plt.show()
 
 
-bodyfat_ajust.to_csv("bodyfat_final.csv", index=False)
+#observe se a coluna de classes precisa ser renomeada para atributos numéricos, realize a conversão, se necessário
+# Criar uma instância do LabelEncoder
+#label_encoder = LabelEncoder()
+# Aplicar o LabelEncoder às colunas categóricas
+#for coluna in bodyfat_ajust:
+    #if bodyfat_ajust[coluna].dtype == 'object':
+        #bodyfat_ajust.loc[:, coluna] = label_encoder.fit_transform(bodyfat_ajust[coluna])
+#atributo mais relevante
+#X = bodyfat_ajust.drop(['BodyFat'], axis=1)
+#y = bodyfat_ajust['BodyFat'].values
+#y = bodyfat_ajust['BodyFat'].values
+#bodyfat_ajust_columns = X.columns
+''# Instantiate a lasso regression model
+#lasso = Lasso(alpha=0.1)
+# Instantiate a lasso regression model
+#lasso = Lasso(alpha=0.3)
+
+# Compute and print the coefficients
+#lasso_coef = lasso.fit(X, y).coef_
+#print(lasso_coef)
+#plt.bar(bodyfat_ajust_columns, lasso_coef)
+#plt.xticks(rotation=45)
+#plt.show()
+
+
+df_final.to_csv("df_final.csv")
 
 #Salve o dataset atualizado se houver modificações.
-bodyfat_ajust.to_csv("bodyfat_final.csv")
+#bodyfat_ajust.to_csv("bodyfat_final.csv")
---
+++ AV2/questão4.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import cross_val_score, KFold
+from sklearn.linear_model import Lasso, Ridge
+from sklearn.preprocessing import LabelEncoder
+
+# Carregar o dataset
+df_final= pd.read_csv("df_final.csv")
+print(df_final)
+
+# Selecionar os atributos relevantes
+relevant_columns = ['Height']
+X = df_final[relevant_columns]
+y = df_final['BodyFat']
+
+# Realizar a codificação de atributos categóricos, se necessário
+label_encoder = LabelEncoder()
+X['Height'] = label_encoder.fit_transform(X['Height'])
+
+# Definir o número de folds para k-fold
+n_folds = 5
+
+# Criar o objeto de k-fold
+kf = KFold(n_splits=n_folds, shuffle=True, random_state=42)
+
+# Realizar a regressão linear com Lasso usando cross-validation
+lasso = Lasso(alpha=0.1)  # Utilize o valor de alpha obtido na questão 4 para Lasso
+lasso_scores = cross_val_score(lasso, X, y, cv=kf)
+print("Scores de cross-validation para Lasso:", lasso_scores)
+print("Média dos scores de cross-validation para Lasso:", np.mean(lasso_scores))
+
+# Realizar a regressão linear com Ridge usando cross-validation
+ridge = Ridge(alpha=0.1)  # Utilize o valor de alpha obtido na questão 4 para Ridge
+ridge_scores = cross_val_score(ridge, X, y, cv=kf)
+print("Scores de cross-validation para Ridge:", ridge_scores)
+print("Média dos scores de cross-validation para Ridge:", np.mean(ridge_scores))
