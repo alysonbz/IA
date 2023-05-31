eac1eabb1a9b6af1113dd51fabe5cf6788e46aa5
ruan rodrigues <ruanrodrigues9ufc@gmail.com>
2023-05-31 02:38:48 -0300

    av2 fim!!!!!! 'ruan_rodrigues' of https://github.com/alysonbz/IA progress.py test edit

--- AV2/questao3.py
+++ AV2/questao3.py
@@ -1,3 +1,5 @@
+import pandas as pd
+import numpy as np
 from sklearn.linear_model import Lasso, Ridge
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
