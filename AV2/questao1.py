8383bf067d83d4b1b7160227ef31b64771eef93e
dcgiovanna <dcgiovanna@alu.ufc.br>
2023-05-31 23:11:21 -0300

    Entrega da prova - 31/05/23

--- AV2/questao1.py
+++ AV2/questao1.py
@@ -1,47 +1,32 @@
 ### Questão 1
 
-#Verifique qual atributo será o alvo para regressão no seu dataset
-#e faça uma análise de qual atributo é mais relevante para realizar a regressão do alvo escolhido.
-#Lembre de comprovar via gráfico.
-#Obs: Registrar na seção de resultados a análise realizada e discutir sobre o resultado encontrado.
-
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.linear_model import Lasso
+import matplotlib.pyplot as plt
 
 df = pd.read_csv(r'C:\Users\Guilherme\Documents\G\IA\AV2\Sample - Superstore.csv', encoding='latin-1')
 
+#Verifique qual atributo será o alvo para regressão no seu dataset
 print(df.columns)
-#print(df.head())
-#print(df.describe())
-print("Info:", df.info())
+print(df.head())
+print(df.info())
 
-#X = df.drop(["Sales"], axis=1).select_dtypes(exclude="object")
-#y = df["Sales"].values
-#sales_columns = df.drop("Sales", axis=1).columns0
-#lasso = Lasso(alpha=0.1)
-#lasso_coef = lasso.fit(X,y).coef_
-#print(lasso_coef)
-#plt.bar(sales_columns, lasso_coef)
-#plt.xticks(rotation=45)
-#plt.show()
+target = 'Profit' #Lucro
 
-
-# Separar os atributos e o alvo
-X = df.drop(['Sales'], axis=1)
-y = df['Sales']
-
-# Instanciar o modelo Lasso
+#Faça uma análise de qual atributo é mais relevante para realizar a regressão do alvo escolhido.
+X = df.drop(target, axis=1).select_dtypes(exclude="object")
+y = df[target].values
+sales_columns = X.columns
 lasso = Lasso(alpha=0.1)
-
-# Ajustar o modelo aos dados
-lasso.fit(X, y)
-
-# Obter os coeficientes
+lasso.fit(X,y)
 lasso_coef = pd.Series(lasso.coef_, index=X.columns)
+print('Importância dos atributos:\n',lasso_coef.sort_values(ascending=False))
 
-# Ordenar os coeficientes em ordem decrescente de importância
-sorted_coef = lasso_coef.abs().sort_values(ascending=False)
+#Comprove via gráfico.
+plt.bar(X.columns, lasso_coef, color="#ff6f9c")
+plt.xlabel('Atributos',fontweight='bold')
+plt.ylabel('Importância',fontweight='bold')
+plt.title('Importância dos atributos na regressão de Profit',fontweight='bold')
+plt.show()
 
-# Exibir os atributos mais importantes
-print(sorted_coef)
+#Obs: Registrar na seção de resultados a análise realizada e discutir sobre o resultado encontrado.
--- AV2/questão4.py
+++ AV2/questão4.py
@@ -1,5 +1,21 @@
 ### Questão 4
 
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import cross_val_score, KFold
+
+df = pd.read_csv(r'C:\Users\Guilherme\Documents\G\IA\AV2\Sample - Superstore.csv', encoding='latin-1')
+
 #Utilizando kfold e cross-validation faça uma regressão linear utilizando os mesmos atributos definidos na questão 3.
+X = df['Sales'].values.reshape(-1, 1)
+y = df['Profit'].values
+kf = KFold(n_splits=6, shuffle=True, random_state=5)
+reg = LinearRegression()
+cv_scores = cross_val_score(reg, X, y, cv=kf)
+print("Scores:\n", cv_scores)
+print("Média: ", np.mean(cv_scores))
+
 #Obs: Com os resultados obtidos na questão 3 e da questão 4 faça uma comparação entre os desempenhos.
 #Escolha o regressor adequado e informe o motivo da escolha. Discuta sobre as limitações e acertos encontrados.
+ 
