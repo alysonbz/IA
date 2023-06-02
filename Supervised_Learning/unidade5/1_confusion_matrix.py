bdfc1518265b5a24382e2d9301596b460865fce0
Maria Bianca Sousa <96076727+Bia14k@users.noreply.github.com>
2023-06-02 10:58:28 -0300

    Update 1_confusion_matrix.py

--- Supervised_Learning/unidade5/1_confusion_matrix.py
+++ Supervised_Learning/unidade5/1_confusion_matrix.py
@@ -1,4 +1,13 @@
-ent/diabetes_clean.csv")
+#Questão 1
+from src.utils import load_diabetes_clean_dataset
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Carregar os dados
+diabetes_df = pd.read_csv(r"/content/diabetes_clean.csv")
 X = diabetes_df.drop(['diabetes'], axis=1)
 y = diabetes_df['diabetes'].values
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42, stratify=y)
@@ -35,3 +44,4 @@ for i in range(cm.shape[0]):
 plt.tight_layout()
 plt.ylabel('Valor Real')
 plt.xlabel('Valor Predito')
+plt.show()
