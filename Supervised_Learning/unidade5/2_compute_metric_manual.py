cd557ecbb5bc113554af98254cc8aca191d599ba
Maria Bianca Sousa <96076727+Bia14k@users.noreply.github.com>
2023-06-02 10:45:41 -0300

    Update 2_compute_metric_manual.py
    tava dando erro pq não atualizei o nome pra c0

--- Supervised_Learning/unidade5/2_compute_metric_manual.py
+++ Supervised_Learning/unidade5/2_compute_metric_manual.py
@@ -1,13 +1,12 @@
 from src.utils import process_diabetes
 
-
 class Metrics:
 
     def __init__(self, y_pred, y_test):
-        self.vp_c1 = 0  #verdadeiro positivo
-        self.vn_c1 = 0  #verdadeiro negativo
-        self.fp_c1 = 0  #falso positivo
-        self.fn_c1 = 0  #falso negativo
+        self.vp_c1 = 0  # verdadeiro positivo
+        self.vn_c1 = 0  # verdadeiro negativo
+        self.fp_c1 = 0  # falso positivo
+        self.fn_c1 = 0  # falso negativo
         self.vp_c0 = 0
         self.vn_c0 = 0
         self.fp_c0 = 0
@@ -26,10 +25,19 @@ class Metrics:
                 self.vn_c1 = self.vn_c1 + 1
 
     def set_param_classe2(self):
-        total_samples = len(self.y_pred)
-        correct_predictions = self.vp_c0 + self.vn_c0 + self.vp_c1 + self.vn_c1
-        accuracy = correct_predictions / total_samples
-        return accuracy
+
+        for yp, yt in zip(y_pred, y_test):
+            if yp == 0 and yt == 0:
+                self.vp_c0 = self.vp_c1 + 1
+
+            if yp == 0 and yt == 1:
+                self.fp_c0 = self.fp_c1 + 1
+
+            if yp == 1 and yt == 0:
+                self.fn_c0 = self.fn_c1 + 1
+
+            if yp == 1 and yt == 1:
+                self.vn_c0 = self.vn_c1 + 1
 
     def compute_acuraccy(self):
         recall_c1 = self.vp_c1 / (self.vp_c1 + self.fn_c1)
@@ -64,7 +72,9 @@ class Metrics:
         return f1_score_c0
 
     def compute_confusion_matriz(self):
-        confusion_matrix = [[self.vp_c0, self.fp_c0], [self.fn_c0, self.vn_c0]], [[self.vp_c1, self.fp_c1],[self.fn_c1, self.vn_c1]]
+        confusion_matrix = [[self.vp_c0, self.fp_c0], [self.fn_c0, self.vn_c0]], [[self.vp_c1, self.fp_c1],
+                                                                                  [self.fn_c1, self.vn_c1]]
+
 
 y_pred, y_test = process_diabetes()
 mt = Metrics(y_pred, y_test)
@@ -72,17 +82,17 @@ mt.set_param_classe1()
 mt.set_param_classe2()
 
 print("acurácia geral:", mt.compute_acuraccy())
-    #
+#
 print("recall classe 0: ", mt.compute_recall_c0)
-    #
+#
 print("recall classe 1: ", mt.compute_recall_c1())
-    #
+#
 print("precision classe 0: ", mt.compute_precision_c0())
-    #
+#
 print("precision classe 1: ", mt.compute_precision_c1())
-    #
+#
 print("F1-score classe 0: ", mt.compute_f1_score_c0())
-    #
+#
 print("F1-score classe 1: ", mt.compute_f1_score_c1())
-    #
+#
 print("Matriz de confusão", mt.compute_confusion_matriz())
