1f962867f544776f2cde6d4ae6a6130ee3bd22fb
ruan_rodrigues <ruanrodrigues9ufc@gmail.com>
2023-06-06 15:03:19 -0300

    aula 16/05

--- Supervised_Learning/unidade5/7_hyperparam_tunig_random_search.py
+++ Supervised_Learning/unidade5/7_hyperparam_tunig_random_search.py
@@ -1,16 +1,18 @@
 import numpy as np
 
+#import kfold
+from sklearn.model_selection import KFold
+
 #import Logistic regression
 from sklearn.linear_model import LogisticRegression
 
 #import train_test_split
 from sklearn.model_selection import train_test_split
 
-#import kfold
-from sklearn.model_selection import KFold
-
-# Import RandomizedSearchCV
+#Import RandomizedSearchCV
 from sklearn.model_selection import RandomizedSearchCV
+
+from sklearn.model_selection import GridSearchCV
 from src.utils import load_diabetes_clean_dataset
 
 diabetes_df = load_diabetes_clean_dataset()
@@ -19,14 +21,15 @@ y = diabetes_df['diabetes'].values
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
 
+
 #inicialize Logistic regression
-logreg = LogisticRegression()
+logreg  = LogisticRegression()
 
 #inicialize kfold
 kf = KFold(n_splits=5, shuffle=True, random_state=42)
 
 #create the parameter space
-params = {"penalty": ["l1", "l2"],
+params = {"penalty": ["l1", "l2", "tol"],
          "tol": np.linspace(0.0001, 1.0, 50),
          "C": np.linspace(0.1, 1, 50),
          "class_weight": ["balanced", {0:0.8, 1:0.2}]}
@@ -39,4 +42,4 @@ logreg_cv.fit(X_train, y_train)
 
 # Print the tuned parameters and score
 print("Tuned Logistic Regression Parameters: {}".format(logreg_cv.best_params_))
-print("Tuned Logistic Regression Best Accuracy Score: {}".format(logreg_cv.best_score_))
+print("Tuned Logistic Regression Best Accuracy Score: {}".format(logreg_cv.best_score))
--- Unsupervised_Learning/unidade6/4_pipeline.py
+++ Unsupervised_Learning/unidade6/4_pipeline.py
@@ -1,37 +1,37 @@
 import pandas as pd
 
 # Perform the necessary imports
-from ____ import ____
-from ____ import ____
-from ____ import ____
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+from sklearn.pipeline import make_pipeline
 
 from src.utils import load_fish_dataset
 
 samples_df = load_fish_dataset()
-samples = samples_df.drop(['specie'],axis=1)
+samples = samples_df.drop(['specie'], axis=1)
 species = samples_df['specie'].values
 
 
 # Create scaler: scaler
-scaler = ____
+scaler = StandardScaler()
 
 # Create KMeans instance: kmeans
-kmeans = ____
+kmeans = KMeans(n_clusters=3)
 
 # Create pipeline: pipeline
-pipeline = ____
+pipeline = make_pipeline(scaler,kmeans)
 
 # Fit the pipeline to samples
-____
+pipeline.fit(samples)
 
 # Calculate the cluster labels: labels
-labels = ____
+labels = pipeline.predict(samples)
 
 # Create a DataFrame with labels and species as columns: df
-df = ____
+df = df = pd.DataFrame({'labels': labels, 'specie': species})
 
 # Create crosstab: ct
-ct = ____
+ct = ct = pd.crosstab(df['labels'],df['specie'])
 
 # Display ct
 print(ct)
