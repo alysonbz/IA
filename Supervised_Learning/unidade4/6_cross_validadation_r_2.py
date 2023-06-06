3e9a156921424864c1451b6944d0618596a6462e
Robertim <robertbarreira@alu.ufc.br>
2023-06-06 13:58:25 -0300

    coisa triste

--- Supervised_Learning/unidade4/6_cross_validadation_r_2.py
+++ Supervised_Learning/unidade4/6_cross_validadation_r_2.py
@@ -13,7 +13,7 @@ y = sales_df["sales"].values
 #Create a KFold object
 kf = KFold(n_splits=6, shuffle=True, random_state=5)
 
-reg = LinearRegression()
+
 
 # Compute 6-fold cross-validation scores
 cv_scores = cross_val_score(reg, X, y, cv=kf)
