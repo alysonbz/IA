f9bd3410d28ab92e9479866adfb68b5099b50cf5
LAB1_14 <williamkelvin@alu.ufc.br>
2023-06-07 17:09:23 -0300

    Prova - Avaliação 2 Finalização!!

--- Unsupervised_Learning/unidade6/3b_cluster_fish.py
+++ Unsupervised_Learning/unidade6/3b_cluster_fish.py
@@ -7,14 +7,15 @@ samples = samples_df.drop(['specie'],axis=1)
 specie = samples_df['specie'].values
 
 # Create KMeans instance: kmeans with 4 custers
-
+model = KMeans(n_clusters=4)
 
 # Use fit_predict to fit model and obtain cluster labels: labels
-
+labels = model.fit_predict(samples)
 
 # Create a DataFrame with labels and varieties as columns: df
-
+df = pd.DataFrame({'labels': labels, 'varietes': specie})
 
 # Create crosstab: ct
-
+df = pd.crosstab(df['labels'], df['varietes'])
 # Display ct
+print(df)
--- Unsupervised_Learning/unidade6/4_pipeline.py
+++ Unsupervised_Learning/unidade6/4_pipeline.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
 # Perform the necessary imports
-from ____ import ____
-from ____ import ____
-from ____ import ____
+from sklearn.preprocessing import StandardScaler
+from  sklearn.cluster import KMeans
+from sklearn.pipeline import make_pipeline
 
 from src.utils import load_fish_dataset
 
@@ -13,25 +13,25 @@ species = samples_df['specie'].values
 
 
 # Create scaler: scaler
-scaler = ____
+scaler = StandardScaler()
 
 # Create KMeans instance: kmeans
-kmeans = ____
+kmeans = KMeans()
 
 # Create pipeline: pipeline
-pipeline = ____
+pipeline = make_pipeline()
 
 # Fit the pipeline to samples
-____
+pipeline.fit(samples)
 
 # Calculate the cluster labels: labels
-labels = ____
+labels = pipeline.predict(samples)
 
 # Create a DataFrame with labels and species as columns: df
-df = ____
+df = pd.DataFrame({'labels': labels, 'varietes': specie })
 
 # Create crosstab: ct
-ct = ____
+ct = ct = pd.crosstab([df] )
 
 # Display ct
 print(ct)
