e9d331784afa5b1b7ac7f2f0efe8d5b4930f0b9d
Bia14k <maria.bianca.mb0@gmail.com>
2023-06-07 15:19:17 -0300

    Merge branch 'main' of https://github.com/alysonbz/IA into Bianca
     Conflicts:
    	Supervised_Learning/unidade4/0_class_anotation.py

--- Unsupervised_Learning/unidade6/3b_cluster_fish.py
+++ Unsupervised_Learning/unidade6/3b_cluster_fish.py
@@ -7,14 +7,15 @@ samples = samples_df.drop(['specie'],axis=1)
 specie = samples_df['specie'].values
 
 # Create KMeans instance: kmeans with 4 custers
-
-
+model = KMeans(n_clusters= 4)
 # Use fit_predict to fit model and obtain cluster labels: labels
-
+labels = model.fit_predict(samples)
 
 # Create a DataFrame with labels and varieties as columns: df
-
+df = pd.DataFrame({"labels" : labels,
+                   "species":specie})
 
 # Create crosstab: ct
-
+ct = pd.crosstab(df["labels"],df["species"])
 # Display ct
+print(ct)
