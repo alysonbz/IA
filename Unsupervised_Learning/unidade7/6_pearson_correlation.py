06919c52fa27c6b0b0e3efddb971fc515f7d654c
Bia14k <maria.bianca.mb0@gmail.com>
2023-06-20 15:05:47 -0300

    Merge branch 'main' of https://github.com/alysonbz/IA into Bianca
     Conflicts:
    	Supervised_Learning/unidade4/0_class_anotation.py

--- Unsupervised_Learning/unidade7/6_pearson_correlation.py
+++ Unsupervised_Learning/unidade7/6_pearson_correlation.py
@@ -4,23 +4,22 @@ import matplotlib.pyplot as plt
 from scipy.stats import pearsonr
 from src.utils import load_grains_dataset
 
-
 grains_df = load_grains_dataset()
 
 
 # Assign the 0th column of grains: width
-width = __
+width = grains_df["0"]
 
 # Assign the 1st column of grains: length
-length = __
+length = grains_df["1"]
 
 # Scatter plot width vs length
-plt.scatter(____, ____)
+plt.scatter(width, length)
 plt.axis('equal')
 plt.show()
 
 # Calculate the Pearson correlation
-correlation, pvalue = ____
+correlation, pvalue = pearsonr(length,width)
 
 # Display the correlation
 print(correlation)
--- Unsupervised_Learning/unidade7/7_manual_pearson_correlation.py
+++ Unsupervised_Learning/unidade7/7_manual_pearson_correlation.py
@@ -1,21 +1,28 @@
 # Perform the necessary imports
+import numpy as np
 import matplotlib.pyplot as plt
 
 from src.utils import load_grains_dataset
 
 
 def pearson_correlation(x,y):
-    return None
+    x_bar= np.mean(x)
+    y_bar= np.mean(y)
+    somatorio = sum((x-x_bar) * (y-y_bar))
+    somatorio2 = sum(((x-x_bar)**2)) * sum((y-y_bar)**2)
+    div = somatorio / (np.sqrt(somatorio2))
+    return div
+
 
 
 grains_df = load_grains_dataset()
 
 
 # Assign the 0th column of grains: width
-width = __
+width = grains_df["0"]
 
 # Assign the 1st column of grains: length
-length = __
+length = grains_df["1"]
 
 # Calculate the Pearson correlation
 correlation = pearson_correlation(width,length)
