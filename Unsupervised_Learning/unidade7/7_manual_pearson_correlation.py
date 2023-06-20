377f3153ea520710166f6269619d2f12e18e217f
alysonribeiro <alysonribeiro@mobitbrasil.com.br>
2023-06-20 10:34:46 -0300

    unidade 7

---
+++ Unsupervised_Learning/unidade7/7_manual_pearson_correlation.py
@@ -0,0 +1,24 @@
+# Perform the necessary imports
+import matplotlib.pyplot as plt
+
+from src.utils import load_grains_dataset
+
+
+def pearson_correlation(x,y):
+    return None
+
+
+grains_df = load_grains_dataset()
+
+
+# Assign the 0th column of grains: width
+width = __
+
+# Assign the 1st column of grains: length
+length = __
+
+# Calculate the Pearson correlation
+correlation = pearson_correlation(width,length)
+
+# Display the correlation
+print(correlation)
