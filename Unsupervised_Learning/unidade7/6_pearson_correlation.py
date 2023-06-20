d86f14a7a176a5c34830b259f1ade025c8243246
Sheldaa <sheldasouza@alu.ufc.br>
2023-06-20 14:52:44 -0300

    modificações aula 20/06.

--- Unsupervised_Learning/unidade7/6_pearson_correlation.py
+++ Unsupervised_Learning/unidade7/6_pearson_correlation.py
@@ -7,20 +7,19 @@ from src.utils import load_grains_dataset
 
 grains_df = load_grains_dataset()
 
-
 # Assign the 0th column of grains: width
-width = __
+width = grains_df['0']
 
 # Assign the 1st column of grains: length
-length = __
+length = grains_df['1']
 
 # Scatter plot width vs length
-plt.scatter(____, ____)
+plt.scatter(width, length)
 plt.axis('equal')
 plt.show()
 
 # Calculate the Pearson correlation
-correlation, pvalue = ____
+correlation, pvalue = pearsonr(width, length)
 
 # Display the correlation
 print(correlation)
