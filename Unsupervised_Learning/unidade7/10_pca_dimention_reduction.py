d7999ac4c6c4e99b346360b8f427e0880645fe97
Robertim <robertbarreira@alu.ufc.br>
2023-06-21 14:51:17 -0300

    acabou amem

--- Unsupervised_Learning/unidade7/10_pca_dimention_reduction.py
+++ Unsupervised_Learning/unidade7/10_pca_dimention_reduction.py
@@ -3,24 +3,33 @@ from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 from src.utils import load_fish_dataset
 from sklearn.preprocessing import LabelEncoder
+import matplotlib.pyplot as plt
 
 samples = load_fish_dataset()
+label_encoder = LabelEncoder()
+specie = label_encoder.fit_transform(samples['specie'])
 samples = samples.drop(['specie'],axis=1)
 scaler = StandardScaler()
 scaled_samples = scaler.fit_transform(samples)
 
 
+
+
 # Create a PCA model with components in adequate number: pca
-pca = __
+pca = PCA(n_components=2)
 
 # Fit the PCA instance to the scaled samples
-__
+pca.fit(scaled_samples)
 
 # Transform the scaled samples: pca_features
-__
+pca_features = pca.transform(scaled_samples)
 
 # Print the shape of pca_features
-__
+print(pca_features.shape)
+
 
 #vizualize scatter plot with dimension reduced
-__
+xs = pca_features[:,0]
+ys = pca_features[:,1]
+plt.scatter(xs,ys, c= specie)
+plt.show()
