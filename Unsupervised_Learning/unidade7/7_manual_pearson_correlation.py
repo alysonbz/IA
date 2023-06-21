a6885f04b658157832ab389a031de9412f52647d
Emily Mendonça <emilymendc@alu.ufc.br>
2023-06-21 15:09:44 +0000

    Atualizando atividades de Ia. 21/06

--- Unsupervised_Learning/unidade7/7_manual_pearson_correlation.py
+++ Unsupervised_Learning/unidade7/7_manual_pearson_correlation.py
@@ -2,23 +2,26 @@
 import matplotlib.pyplot as plt
 
 from src.utils import load_grains_dataset
+import numpy as np
 
 
-def pearson_correlation(x,y):
-    return None
-
+def placa_mae(x,y):
+    erick = sum((np.array(x) - np.mean(x)) * (np.array(y)-np.mean(y)))
+    shelda = np.sqrt(sum((np.array(x) - np.mean(x))**2)*(sum((np.array(y)-np.mean(y))**2)))
+    emy = erick/shelda
+    return emy
 
 grains_df = load_grains_dataset()
 
 
 # Assign the 0th column of grains: width
-width = __
+width = grains_df['0']
 
 # Assign the 1st column of grains: length
-length = __
+length = grains_df['1']
 
 # Calculate the Pearson correlation
-correlation = pearson_correlation(width,length)
+correlation = placa_mae(width,length)
 
 # Display the correlation
 print(correlation)
