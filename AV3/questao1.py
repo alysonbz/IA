96ce05e23e93af1e0421f4ef8da3c393fe3b5124
Lucas do Nascimento <lucasnascimentosousa@alu.ufc.br>
2023-06-30 14:13:18 -0300

    AV3

--- AV3/questao1.py
+++ AV3/questao1.py
@@ -1,24 +1,60 @@
+#importe as bibliotecas necessárias
 import pandas as pd
+import numpy as np
+from sklearn.preprocessing import normalize
+from scipy.cluster.hierarchy import linkage, dendrogram
 import matplotlib.pyplot as plt
-from scipy.cluster.hierarchy import dendrogram, linkage
-
-test = pd.read_csv("test_dataset.csv")
-
-test = test[['Area', 'Perimeter', 'ConvexArea', 'MajorAxisLength']]
-
-X_train, samples, y_train, varieties = test
+from scipy.cluster.hierarchy import fcluster
+from sklearn.cluster import KMeans
 
 
-# Calculate the linkage: mergings
-mergings = linkage(samples, method='complete')
+## Carregar o dataset
+test_df = pd.read_csv('test_dataset.csv')
 
-# Plot the dendrogram, using varieties as labels
+
+# Verificar se tem Na ou isnull
+'''
+print(test_df.isna().sum()
+print(test_df.isnull().sum())
+'''
+
+
+
+# DENDOGRAMA
+test = test_df.drop(['Area'], axis=1)
+Area = test_df['Area'].values
+
+
+# Normalizar os dados
+normalized_test = normalize(test)
+
+
+# Calcular o linkage: mergings
+mergings = linkage(normalized_test, method='complete')
+# Plotar o dendrograma
+plt.figure(figsize=(10, 6))
 dendrogram(mergings,
-           labels=varieties,
+           labels=Area,
            leaf_rotation=90,
-           leaf_font_size=6,
-)
+           leaf_font_size=8)
+
+plt.title('Dendrograma')
+plt.xlabel('Amostras')
+plt.ylabel('Distância')
 plt.show()
+
+
+# clusterização e aplique o k-medias
+model = KMeans(n_clusters=3)
+
+# Use fit_predict em model
+labels = model.fit_predict(test)
+# Create a DataFrame with labels and varieties as columns: df
+df = pd.DataFrame({'labels': labels, 'Area': Area})
+# Create crosstab: ct
+ct = pd.crosstab(df['labels'], df['Area'])
+# Display ct
+print(ct)
 
 
 
---
+++ AV3/questao3.py
@@ -0,0 +1,51 @@
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report, confusion_matrix
+import pandas as pd
+
+
+# Carregar o Dataset
+test_df = pd.read_csv('test_dataset.csv')
+test = test_df.drop(['Area'],axis=1)
+Area = test_df['Area'].values
+
+
+# Criar um PCA
+pca = PCA(n_components=2)
+scaled_test = pca.fit_transform(test)
+
+# Reduzir a dimensionalidade usando t-SNE
+model = TSNE(n_components=2)
+Normalized_test = model.fit_transform(test)
+
+# Dividir os dados reduzidos em treinamento e teste
+pca_train, pca_test, y_train1, y_test1 = train_test_split(Normalized_test, Area, test_size=0.2, random_state=42)
+tsne_train, tsne_test, y_train, y_test = train_test_split(Normalized_test, Area, test_size=0.2, random_state=42)
+
+# Criar classificadores k-NN para PCA e t-SNE
+knn_pca = KNeighborsClassifier(n_neighbors=3)
+knn_tsne = KNeighborsClassifier(n_neighbors=3)
+
+# Treinar os classificadores usando os dados de treinamento
+knn_pca.fit(pca_train, y_train1)
+knn_tsne.fit(tsne_train, y_train)
+
+# Fazer previsões sobre os dados de teste
+y_pred_pca = knn_pca.predict(pca_test)
+y_pred_tsne = knn_tsne.predict(tsne_test)
+
+# Calcular métricas para o PCA
+print("classification_report para PCA:")
+print(classification_report(y_test, y_pred_pca))
+print("confusion_Matrix para PCA:")
+print(confusion_matrix(y_test, y_pred_pca))
+
+# Calcular métricas para t-SNE
+print("classification_report para t-SNE:")
+print(classification_report(y_test, y_pred_tsne))
+print("confusion_Matrix para t-SNE:")
+print(confusion_matrix(y_test, y_pred_tsne))
+
+
