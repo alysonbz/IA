f52153834d9cbb0280c556cd9a961858bcf4f0ba
Bia14k <maria.bianca.mb0@gmail.com>
2023-07-04 14:01:24 -0300

    Merge branch 'main' of https://github.com/alysonbz/IA into Bianca
     Conflicts:
    	Supervised_Learning/unidade4/0_class_anotation.py

--- Unsupervised_Learning/unidade7/11_apply_no_supervised_tec.py
+++ Unsupervised_Learning/unidade7/11_apply_no_supervised_tec.py
@@ -1,60 +1,57 @@
-#bibliotecas necessárias
-from src.utils import load_fish_dataset
-from sklearn.decomposition import PCA
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import StandardScaler
-from sklearn.pipeline import make_pipeline
 import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from src.utils import load_fish_dataset
+from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
-import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 
-#inicialização
-pca = PCA()
-knn = KNeighborsClassifier()
+# lendo o dataset
+samples = load_fish_dataset()
+
+# Codificar a variável igual a Thaís
 lb = LabelEncoder()
+especie_encoded = lb.fit_transform(samples["specie"])
+
+samples = samples.drop(['specie'], axis=1)
+
+# Padronizar os atributos
 scaler = StandardScaler()
+scaled_samples = scaler.fit_transform(samples)
 
-#carregador
-fish_df = load_fish_dataset()
-specie_encoded = lb.fit_transform(fish_df['specie'])
-fish_df = fish_df.drop(['specie'],axis=1)
-y = fish_df['specie'].values
-scaled_fish = scaler.fit_transform(fish_df)
+# Dividir o conjunto de dados em treinamento e teste
+X_train, X_test, y_train, y_test = train_test_split(scaled_samples, especie_encoded, test_size=0.2, random_state=42)
 
-# redução da dimensão com PCA do dataset
-
-pipeline = make_pipeline(scaler,pca)
-pipeline.fit(fish_df)
-features = range(pca.n_components_)
-plt.bar(features, pca.explained_variance_)
-plt.xlabel('PCA feature')
-plt.ylabel('variance')
-plt.xticks(features)
-plt.show()
-
-#foi obtido o gráfico com as principais variancias da coluna especies, duas se sobressairam, com base nisso vamos análisar estes atributos, agora fica fácil de fazer essse processo, já que sabemos que apenas dois ficam acima de 1,5.
+# instânciar o PCA
 pca = PCA(n_components=2)
-pca.fit(scaled_fish)
-transformed = pca.transform(scaled_fish)
-xs = transformed[:,0]
-ys = transformed[:,1]
-print(transformed.shape)
-plt.scatter(xs,ys, c= specie_encoded)
+
+pca.fit(X_train)
+
+# Transformar os dados de treinamento e teste com PCA
+X_train_pca = pca.transform(X_train)
+X_test_pca = pca.transform(X_test)
+
+# usar o classificador do Henrique
+clf = RandomForestClassifier()
+clf.fit(X_train_pca, y_train)
+
+# Predict
+y_pred = clf.predict(X_test_pca)
+
+accuracy = accuracy_score(y_test, y_pred)
+print("Acurácia:", accuracy)
+
+# classification report
+report = classification_report(y_test, y_pred)
+print("Classification Report:")
+print(report)
+
+#matriz de confusão
+confusion_mat = confusion_matrix(y_test, y_pred)
+print("Matriz de Confusão:")
+print(confusion_mat)
+
+plt.scatter(X_test_pca[:, 0], X_test_pca[:, 1], c=y_pred)
+plt.title("Scatter Plot - PCA")
 plt.show()
-
-#depois do processo do PCA, será escolhido um classificador, assim classificando os atributos que já foram gerados anteriormente, neste caso o classificador especifico usado foi o Knn.
-#divisão em teste e treino
-fish_df_train, fish_df_test, y_train, y_test = train_test_split(fish_df, y, stratify=y, random_state=42)
-#ajustar o modelo(treino)
-knn.fit(fish_df_train, y_train)
-#acerto (teste)
-print(knn.score(fish_df_test,y_test))
-ac = str(round(knn.score(fish_df_test,y_test) * 100, 2))+"%"
-print("A acurácia do modelo k-NN foi",ac)
-
-
-
-
-
-
