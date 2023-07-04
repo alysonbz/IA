097f76f3ea641d8d3e5c2dbd1e306b823f68d5cc
Bia14k <maria.bianca.mb0@gmail.com>
2023-07-04 15:20:58 -0300

    Merge branch 'main' of https://github.com/alysonbz/IA into Bianca
     Conflicts:
    	Supervised_Learning/unidade4/0_class_anotation.py

--- AV3/questao4.py
+++ AV3/questao4.py
@@ -1,8 +1,8 @@
-
+import numpy as np
 import pandas as pd
 from sklearn.decomposition import PCA
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
 # Lendo o dataset
@@ -21,7 +21,7 @@ X_pca = pca.fit_transform(X)
 X_train, X_test, y_train, y_test = train_test_split(X_pca, y, test_size=0.2, random_state=0)
 
 # Treinamento do classificador
-classifier = LogisticRegression()
+classifier = KNeighborsClassifier()
 classifier.fit(X_train, y_train)
 
 # Previsão dos rótulos
