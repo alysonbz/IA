9960d432c8555d66d737ed8432be8d589464a40c
Ruan Rodrigues 10 <104784848+RuanRodrigues10000@users.noreply.github.com>
2023-07-05 08:17:50 -0300

    Create Questão3.py
    Av3

---
+++ AV3/Questão3.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+# Carregar o dataset
+df = pd.read_csv(r"C:\Users\ruanr\OneDrive\Área de Trabalho\IA\mitbih_train.csv")
+
+# Pré-processamento dos dados
+X = df.drop('9.779411554336547852e-01', axis=1)
+y = pd.cut(df['9.779411554336547852e-01'], bins=[-float('inf'), 0.5, float('inf')], labels=['Normal', 'Abnormal'])
+
+# Divisão do conjunto de dados
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Aplicação do classificador (Random Forest)
+rf = RandomForestClassifier(random_state=42)
+rf.fit(X_train, y_train)
+y_pred = rf.predict(X_test)
+
+# Avaliação do desempenho
+accuracy = accuracy_score(y_test, y_pred)
+
+# Resultado
+print("Acurácia:", accuracy)
