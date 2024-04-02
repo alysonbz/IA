799ee236b3ffcb3b4f10c24471afca1aa0a1e729
Juraneto <jurandircavalcanti@alu.ufc.br>
2024-04-02 13:42:00 -0300

    q2

--- Pre-processing/Unidade1/1_pre_processing.py
+++ Pre-processing/Unidade1/1_pre_processing.py
@@ -1,23 +1,39 @@
+
+import pandas as pd
 from src.utils import load_volunteer_dataset
 
 volunteer = load_volunteer_dataset()
 
 # Mostre a dimensão do dataset volunteer
-print(___)
+print("DIMENSÃO")
+print(volunteer.ndim)
+print()
 
 #mostre os tipos de dados existentes no dataset
-___
+print("TIPOS DE DADOS")
+print(volunteer.dtypes)
+print()
 
 #mostre quantos elementos do dataset estão faltando na coluna
----
+print("ELEMENTOS NaN")
+print(volunteer.isna().sum())
+print()
 
 # Exclua as colunas Latitude e Longitude de volunteer
-volunteer_cols =
+print("TODAS AS COLUNAS", volunteer.columns)
+
+volunteer_cols = volunteer.drop(["Latitude", "Longitude"], axis=1)
+
+print("APÓS REMOVER AS COLUNAS", volunteer_cols.columns)
+print()
 
 # Exclua as linhas com valores null da coluna category_desc de volunteer_cols
-volunteer_subset =
+volunteer_cols.dropna(subset=["category_desc"], inplace=True)
+print(volunteer_cols.isna().sum())
+print()
 
 # Print o shape do subset
-___
+print(volunteer.shape)
+print("Linhas:", volunteer.shape[0], "Colunas:", volunteer.shape[-1])
 
 
