a1c1da1637bb6cd7004e052f7d6e6db890809e3a
wandersonwendel <wandsolopes@alu.ufc.br>
2024-04-02 09:41:08 -0300

    Atividade 2 concluída - '0_class_anotation'.

--- Pre-processing/Unidade1/0_class_anotation.py
+++ Pre-processing/Unidade1/0_class_anotation.py
@@ -7,4 +7,49 @@ wine  = load_wine_dataset()
 df1 = load_df1_unidade1()
 df2 = load_df2_unidade1()
 
+# hiking = pd.read_json("hiking.json")
+print("DF1")
+print(hiking.head())
+print("\n")
 
+print(hiking.info())
+print("\n")
+
+print(wine.describe())
+print("\n")
+
+print(df1)
+print(df1.dropna())
+print("\n")
+
+print(df1)
+print(df1.drop([1, 2, 3]))
+print("\n")
+
+print(df1)
+print(df1.drop("A", axis=1))
+print("\n")
+
+print(df1)
+print(df1.isna().sum())
+print(df1.dropna(subset=["B"]))
+print("\n")
+
+print(df1)
+print(df1.dropna(thresh=2))
+print("\n")
+
+print(df1)
+print(volunteer.info())
+print("\n")
+
+print("DF2")
+print(df2)
+print("\n")
+
+print(df2.info())
+print("\n")
+
+df2["C"] = df2["C"].astype("int64")
+print(df2.dtypes)
+print("\n")
