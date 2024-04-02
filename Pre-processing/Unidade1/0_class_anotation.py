631d3be707c28f971d75ad5b7b30ff4c08da6a4e
pinheiroiwnll <pinheiroiwnl@alu.ufc.br>
2024-04-02 09:50:56 -0300

    class anotation

--- Pre-processing/Unidade1/0_class_anotation.py
+++ Pre-processing/Unidade1/0_class_anotation.py
@@ -7,4 +7,26 @@ wine  = load_wine_dataset()
 df1 = load_df1_unidade1()
 df2 = load_df2_unidade1()
 
+print(df1)
+print(df1.dropna())
 
+print(df1)
+print(df1.drop([1, 2, 3]))
+
+print(df1)
+print(df1.drop("A", axis=1))
+
+print(df1)
+print(df1.isna().sum())
+print(df1.dropna(subset=["B"]))
+
+print(df1)
+print(df1.dropna(thresh=2))
+
+print(volunteer.info())
+
+print(df2)
+
+print(df2.info())
+
+print(df2.dtypes)
