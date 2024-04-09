36d335b509389839426eb09543e96e4cdc09608b
sousafilp <sousaafilp@gmail.com>
2024-04-09 08:43:59 -0300

    unidade 1

--- Pre-processing/Unidade1/2_data_types.py
+++ Pre-processing/Unidade1/2_data_types.py
@@ -3,13 +3,17 @@ from src.utils import load_volunteer_dataset
 volunteer = load_volunteer_dataset()
 
 # Print os primeiros elementos da coluna hits
-___
+print(volunteer["hits"])
+print()
 
 # Print as caracteristicas da coluna hits
-__
+print("Características da coluna hits:", volunteer["hits"].dtypes)
+print()
 
 # Converta a coluna hits para o tipo int
-___
+volunteer_int = volunteer["hits"].astype("int32")
+print()
 
 # Print as caracteristicas da coluna hits novamente
----
+print("Coluna volunteer convertida para:", volunteer_int.dtypes)
+print(volunteer_int.dtypes)
--- Pre-processing/Unidade2/1_standardization.py
+++ Pre-processing/Unidade2/1_standardization.py
@@ -9,12 +9,12 @@ X = wine.drop(['Quality'],axis=1)
 y = wine['Quality'].values
 
 # divida o dataset em treino e teste
-X_train, X_test, y_train, y_test = ____(____, ____, stratify=____, random_state=42)
+X_train, x_test, y_train, y_test = train_test_split(X, y, stratify=y, random_state=42)
 
 knn = KNeighborsClassifier()
 
 # Aplique a função fit do knn
-knn.____(____, ____)
+knn.fit(x_test, y_test)
 
 # mostre o acerto do algoritmo
-print(knn.____(____))
+print(knn.score(x_test, y_test))
