7c97a9847798a51e0ddb2804f0027cbf907190c1
samuelpinheiro1 <ggkakashi2001@gmail.com>
2024-07-30 08:39:59 -0300

    Todas as entregas

--- Supervised_Learning/unidade3/1_k-Nearest-Neighbors-Fit.py
+++ Supervised_Learning/unidade3/1_k-Nearest-Neighbors-Fit.py
@@ -1,27 +1,25 @@
 from src.utils import load_churn_dataset
 import numpy as np
-
-# Import KNeighborsClassifier
-from ____.____ import ____
+from sklearn.neighbors import KNeighborsClassifier  # Importing KNeighborsClassifier from sklearn.neighbors
 
 churn_df = load_churn_dataset()
 
 # Create arrays for the features and the target variable
-y = ____["____"].values
-X = ____[["____", "____"]].values
+y = churn_df["Churn"].values  # Assuming "Churn" is the name of the target variable column
+X = churn_df[["TotalCharges", "MonthlyCharges"]].values  # Assuming these are the feature columns
 
 # Create a KNN classifier with 6 neighbors
-knn = ____
+knn = KNeighborsClassifier(n_neighbors=6)
 
 # Fit the classifier to the data
-knn.____(____, ____)
+knn.fit(X, y)
 
 X_test = np.array([[30.0, 17.5],
-                  [107.0, 24.1],
-                  [213.0, 10.9]])
+                   [107.0, 24.1],
+                   [213.0, 10.9]])
 
-# Predict the labels for the X_teste
-y_pred = __.__(__)
+# Predict the labels for the X_test
+y_pred = knn.predict(X_test)
 
 # Print the predictions for X_test
-print("Predictions: {}".format(__))
+print("Predictions: {}".format(y_pred))
--- Supervised_Learning/unidade3/3_underfiting_overfiting.py
+++ Supervised_Learning/unidade3/3_underfiting_overfiting.py
@@ -5,42 +5,43 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 churn_df = load_churn_dataset()
-X = churn_df[["account_length",  "total_day_charge" , "total_eve_charge",  "total_night_charge","total_intl_charge","number_customer_service_calls"]].values
+X = churn_df[["account_length", "total_day_charge", "total_eve_charge", "total_night_charge", "total_intl_charge",
+              "number_customer_service_calls"]].values
 y = churn_df["churn"].values
 
 # Split into training and test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
 
 # Create neighbors
-neighbors = np.arange(____, ____)
+neighbors = np.arange(1, 26)
 train_accuracies = {}
 test_accuracies = {}
 
 for neighbor in neighbors:
     # Set up a KNN Classifier
-    knn = ____(____=____)
+    knn = KNeighborsClassifier(n_neighbors=neighbor)
 
     # Fit the model
-    knn.____(____, ____)
+    knn.fit(X_train, y_train)
 
     # Compute accuracy
-    train_accuracies[____] = knn.____(____, ____)
-    test_accuracies[____] = knn.____(____, ____)
+    train_accuracies[neighbor] = knn.score(X_train, y_train)
+    test_accuracies[neighbor] = knn.score(X_test, y_test)
 
-print("acuracy on train: ",train_accuracies, '\n',"acuracy on test: ", test_accuracies)
+print("acuracy on train: ", train_accuracies, '\n', "acuracy on test: ", test_accuracies)
 
 # Add a title
-plt.title("____")
+plt.title("KNN: Varying Number of Neighbors")
 
-# Plot training accuracies
-plt.plot(____, ____, label="____")
+#  Plot training accuracies
+plt.plot(neighbors, train_accuracies.values(), label="Training Accuracy")
 
 # Plot test accuracies
-plt.plot(____, ____, label="____")
+plt.plot(neighbors, test_accuracies.values(), label="Testing Accuracy")
 
 plt.legend()
 plt.xlabel("Number of Neighbors")
 plt.ylabel("Accuracy")
 
 # Display the plot
-____
+plt.show()
