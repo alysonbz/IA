0569f3304d55123be454fae937ecb3f1014a8044
juraneto <juraneto1531@gmail.com>
2024-08-06 18:57:53 -0300

    questao 1 prova

--- AV1/questao1.py
+++ AV1/questao1.py
@@ -1,24 +1,23 @@
-#importe as bibliotecas necessárias
-
+# importe as bibliotecas necessárias
+import pandas as pd
 
 ## Carregue o dataset definido para você
-
-
+diabetes = pd.read_csv('AV1/dataset/diabetes.csv')
+print(diabetes.head())
 
 # Verifique se existem celulas vazias ou Nan. Se existir, excluir e criar um novo dataframe.
-
-
+diabetes_limpo = diabetes.dropna() 
 
 # Verifique quais colunas são as mais relevantes e crie um novo dataframe.
+diabetes_relevante = diabetes_limpo[['Outcome', 'Age', 'BloodPressure', 'Glucose']]
 
+# Print o dataframe final e mostre a distribuição de classes que você deve classificar
+print(diabetes_relevante.head())
+print(diabetes_relevante['Outcome'].value_counts())
 
+# Observe se a coluna de classes precisa ser renomeada para atributos numéricos, realize a conversão, se necessário
+# Se a coluna 'Outcome' tiver valores como 'Sim' e 'Não', você pode converter para 1 e 0:
+diabetes_relevante['Outcome'] = diabetes_relevante['Outcome'].replace({1: 'Sim', 0: 'Não'})
 
-#Print o dataframe final e mostre a distribuição de classes que você deve classificar
-
-
-
-#observe se a coluna de classes precisa ser renomeada para atributos numéricos, realize a conversão, se necessário
-
-
-
-#Salve o dataset atualizado se houver modificações.
+# Salve o dataset atualizado se houver modificações.
+diabetes_relevante.to_csv('diabetes_atualizado.csv', index=False) 
