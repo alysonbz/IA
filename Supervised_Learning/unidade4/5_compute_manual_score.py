94e44100bb119ccf912c02de4dda20c2218f0d6e
wandersonwendel <wandsolopes@alu.ufc.br>
2024-08-20 09:33:55 -0300

    Atividades 1 a 5, unidade 4

--- Supervised_Learning/unidade4/5_compute_manual_score.py
+++ Supervised_Learning/unidade4/5_compute_manual_score.py
@@ -2,23 +2,23 @@ import numpy as np
 from src.utils import processing_all_features_sales_clean
 
 def compute_RSS(predictions,y):
-    RSS = None
+    RSS = np.sum((y - predictions)**2)
     return RSS
 def compute_MSE(predictions,y):
-    MSE= None
+    MSE= np.mean((y - predictions) ** 2)
     return MSE
 def compute_RMSE(predictions,y):
-    RMSE = None
+    RMSE = np.sqrt(compute_MSE(predictions, y))
     return RMSE
 def compute_R_squared(predictions,y):
-    r_squared = None
+    var_pred = np.sum(np.square(predictions - np.mean(y)))
+    var_data = np.sum(np.square(y - np.mean(y)))
+    r_squared = np.divide(var_pred, var_data)
     return r_squared
 
-
 X,y,predictions = processing_all_features_sales_clean()
-
 
 print("RSS: {}".format(compute_RSS(predictions,y)))
 print("MSE: {}".format(compute_MSE(predictions,y)))
 print("RMSE: {}".format(compute_RMSE(predictions,y)))
-print("R^2: {}".format(compute_R_squared(predictions,y)))
+print("R²: {}".format(compute_R_squared(predictions,y)))
