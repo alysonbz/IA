a44effa2111f9d90a9330ebd3b2e846496e2b831
pinheiroiwnll <pinheiroiwnl@alu.ufc.br>
2024-08-23 11:02:21 -0300

    Atividade Unidade 4

--- Supervised_Learning/unidade4/7_ridge_regularization.py
+++ Supervised_Learning/unidade4/7_ridge_regularization.py
@@ -1,25 +1,25 @@
 from src.utils import load_sales_clean_dataset
 from sklearn.model_selection import train_test_split
 # Import Ridge
-from ____.____ import ____
+from sklearn.linear_model import Ridge
 
 
 sales_df = load_sales_clean_dataset()
 # Create X and y arrays
 X = sales_df.drop(["sales","influencer"], axis=1)
 y = sales_df["sales"].values
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42,stratify=y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
 alphas = [0.1, 1.0, 10.0, 100.0, 1000.0, 10000.0]
 ridge_scores = []
 for alpha in alphas:
     # Create a Ridge regression model
-    ridge = ____
+    ridge = Ridge(alpha=alpha)
 
     # Fit the data
-    ____
+    ridge.fit(X_train, y_train)
 
     # Obtain R-squared
-    score = ____
+    score = ridge.score(X_test, y_test)
     ridge_scores.append(score)
 print(ridge_scores)
