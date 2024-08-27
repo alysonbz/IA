0721b8cbfe982416bbf4d6b95f3589d4bfa8da33
sousafilp <sousaafilpp@gmail.com>
2024-08-26 22:49:48 -0300

    atividade de casa

--- Supervised_Learning/unidade4/9_manual_ cross_validation.py
+++ Supervised_Learning/unidade4/9_manual_ cross_validation.py
@@ -1,52 +1,61 @@
 import numpy as np
 from src.utils import load_sales_clean_dataset
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
 
 
 class KFold:
 
-   def __init__(self,n_splits):
+    def __init__(self, n_splits):
+        self.n_splits = n_splits
 
-       self.n_splits = n_splits
+    def _compute_score(self, obj, X_train, y_train, X_test, y_test):
+        obj.fit(X_train, y_train)
+        y_pred = obj.predict(X_test)
+        score = mean_squared_error(y_test, y_pred)  # Usa o erro quadrático médio como métrica de score
+        return score
 
-   def _compute_score(self,X,y):
-       return None
-
-   def cross_val_score(self,obj,X, y):
-
+    def cross_val_score(self, obj, X, y):
         scores = []
+        fold_size = len(X) // self.n_splits
 
-        # parte 1: dividir o dataset X em n_splits vezes
+        for i in range(self.n_splits):
+            start = i * fold_size
+            end = start + fold_size
 
-        # parte 2: Calcular a métrica score para subset dividida na parte 1. Chamar a função _compute_score para cada subset
+            X_test = X[start:end]
+            y_test = y[start:end]
 
-        #appendar na lista scores cada valor obtido na parte 2
+            X_train = np.concatenate([X[:start], X[end:]])
+            y_train = np.concatenate([y[:start], y[end:]])
 
-        #parte 3 - retornar a lista de scores
+            score = self._compute_score(obj, X_train, y_train, X_test, y_test)
+            scores.append(score)
 
         return scores
 
 
+# Carrega o dataset
 sales_df = load_sales_clean_dataset()
 
-# Create X and y arrays
+# Cria os arrays X e y
 X = sales_df["tv"].values.reshape(-1, 1)
 y = sales_df["sales"].values
 
-# Create a KFold object
+# Cria um objeto KFold
 kf = KFold(n_splits=6)
 
+# Cria o modelo de regressão linear
 reg = LinearRegression()
 
-# Compute 6-fold cross-validation scores
-cv_scores = kf.cross_val_score(reg,X, y)
+# Calcula os scores de validação cruzada com 6 folds
+cv_scores = kf.cross_val_score(reg, X, y)
 
-# Print scores
+# Imprime os scores
 print(cv_scores)
 
-# Print the mean
+# Imprime a média dos scores
 print(np.mean(cv_scores))
 
-# Print the standard deviation
+# Imprime o desvio padrão dos scores
 print(np.std(cv_scores))
-
