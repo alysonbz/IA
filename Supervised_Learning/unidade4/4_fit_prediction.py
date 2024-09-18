836dc1a07c1a27124116023de2565d6abfdb9345
samuelpinheiro1 <ggkakashi2001@gmail.com>
2024-09-17 23:43:28 -0300

    Novas atividades

--- Supervised_Learning/unidade4/4_fit_prediction.py
+++ Supervised_Learning/unidade4/4_fit_prediction.py
@@ -1,33 +1,34 @@
 from src.utils import load_sales_clean_dataset
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+import numpy as np
+
 sales_df = load_sales_clean_dataset()
 
-# Import mean_squared_error
-from ____.____ import ____
-
 # Create X and y arrays
-X = sales_df.____(["____","___" ],axis=____)
-y = sales_df["____"].____
+X = sales_df.drop(["sales", "social_media", "influencer"], axis=1)
+y = sales_df["sales"].values
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
 # Instantiate the model
-reg = ____
+reg = LinearRegression()
 
 # Fit the model to the data
-____
+reg.fit(X_train, y_train)
 
 # Make predictions
-y_pred = reg.____(____)
+y_pred = reg.predict(X_test)
 print("Predictions: {}, Actual Values: {}".format(y_pred[:2], y_test[:2]))
 
 # Compute R-squared
-r_squared = reg.____(____, ____)
+r_squared = reg.score(X_test, y_test)
 
 # Compute RMSE
-rmse = ____(____, ____, squared=____)
+mse = mean_squared_error(y_test, y_pred)  # Compute MSE
+rmse = np.sqrt(mse)  # Compute RMSE by taking the square root of MSE
 
 # Print the metrics
-print("R^2: {}".format(____))
-print("RMSE: {}".format(____))
+print("R^2: {}".format(r_squared))
+print("RMSE: {}".format(rmse))
